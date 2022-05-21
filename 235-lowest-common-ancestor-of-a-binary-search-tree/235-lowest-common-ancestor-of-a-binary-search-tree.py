6af2da86505014c55c0d20d87c38faa1961b8874
Shiva Acharya <cva.acharya8@gmail.com>
2022-05-20 18:01:24 -0700

    Time: 82 ms (85.69%), Space: 18.7 MB (97.42%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,80 +8,14 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        ############# RECURSIVE APPROACH #######################
-        """
-        # Make p the lowest
-        if q.val < p.val:
-            p, q = q, p
-            
-        def lca(root, p, q):
-            if not root:
-                return None
-            
-            # If root's value is inbetween p and q, return root
-            if p.val < root.val < q.val:
-                return root
-            
-            if root is p or root is q:
-                return root
-            
-            return lca(root.left, p, q) or lca(root.right, p, q)
-        
-        return lca(root, p, q)
-        """
-        ################# ITERATIVE APPROACH ########################
-        """
-        # Make p the lowest
-        if q.val < p.val:
-            p, q = q, p
         while root:
             if p.val < root.val < q.val:
                 return root
-            
-            if root is p or root is q:
+            if q.val < root.val < p.val:
                 return root
-            
-            if root.val < p.val:
+            if root.val == p.val or root.val == q.val:
+                return root
+            if p.val > root.val and q.val > root.val:
                 root = root.right
             else:
                 root = root.left
-        """
-        #################### WITHOUT USING BST PROPERTY ##################
-        if not root:
-            return None
-        
-        if root is p or root is q:
-            return root
-        
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        
-        if not left and not right:
-            return None
-        
-        if left and right:
-            return root
-        
-        return left or right
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        

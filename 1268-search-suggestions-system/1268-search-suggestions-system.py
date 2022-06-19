b47f984ce15d28476b77e9cd639cee6f46fc0a48
Shiva Acharya <cva.acharya8@gmail.com>
2022-06-19 11:50:03 -0700

    Time: 169 ms (64.76%), Space: 17.2 MB (49.83%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -22,8 +22,8 @@ class Solution:
         for chr in searchWord:
             tmp = []
             input_char += chr
-            insertion_index = self.binary_search(products, input_char) # find where input_char can be inserted in-order in the products array
-            for word_ind in range(insertion_index, min(array_len, insertion_index+3)): # check the following 3 words, if valid
+            self.binary_search(products, input_char) # find where input_char can be inserted in-order in the products array
+            for word_ind in range(self.lo, min(array_len, self.lo + 3)): # check the following 3 words, if valid
                 if products[word_ind].startswith(input_char):
                     tmp.append(products[word_ind])
             ans.append(tmp)

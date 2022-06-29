fb4acac9b00aeb02f5f7ee93edcc68e2af383ef1
Shiva Acharya <cva.acharya8@gmail.com>
2022-06-28 19:05:34 -0700

    Time: 86 ms (30.67%), Space: 14.7 MB (40.44%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,11 +1,11 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         # Handle edge cases
-        if target < nums[0]: return 0
-        if target > nums[-1]: return len(nums)
+        # if target < nums[0]: return 0
+        # if target > nums[-1]: return len(nums)
         
         # Binary Search
-        l, r = 0, len(nums) - 1
+        l, r = 0, len(nums)
         
         while l < r:
             mid = (l + r) // 2

9026837e6e7c8c8028a3f63d6294faa38b904dfb
Shiva Acharya <cva.acharya8@gmail.com>
2022-06-29 17:31:59 -0700

    Time: 136 ms (33.86%), Space: 14.1 MB (74.91%) - LeetHub

---
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        
+        freq_map = {}
+        l = max_length = 0
+        
+        for r, c in enumerate(s):
+            
+            freq_map[c] = freq_map.get(c, 0) + 1
+            
+            while len(freq_map) > k:
+                left_char = s[l]
+                freq_map[left_char] -= 1
+                
+                if not freq_map[left_char]:
+                    del freq_map[left_char]
+                
+                l += 1
+            
+            max_length = max(max_length, r - l + 1)
+        
+        return max_length
+                
+        
+        

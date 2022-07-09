60f6d802bee3719b4c257ba2bac42714ded7fe33
Shiva Acharya <cva.acharya8@gmail.com>
2022-07-09 11:30:28 -0700

    Time: 1010 ms (13.55%), Space: 15.2 MB (71.28%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,36 +1,89 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        """
+        Example 1:
+        Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
         
-        wordList = set(wordList)
+        hit => log
         
-        # Handle edge case
-        if endWord not in wordList: return 0
+        [hit, hot, dot, dog, log]
         
-        # Initialize Queue, changes and Visited set
-        Q = deque([beginWord])
-        visited = set([beginWord])
-        changes = 1
+        hit => hot => dot => dog => cog => log  
+        
+        Example 2:
+        Input: beginWord = "a", endWord = "p", wordList = ['b', 'c', 'z']
+        Return 0
+        
+        BFS, Queue = [(hot, 1), (dot, 2), (lot, 2)]
+        
+        word, level = (hot, 1)
+        
+        Hashset => wordList
+        Process word Hashset
+        
+        {hot, dot, cog}
+        
+        1. Initialize a hashset for the wordlist.
+        2. Initialize a Queue with (beginword, 1).
+        3. Dequeue a word from the queue and process the word. Add new word to the queue if in wordlist.
+        4. Return the level if endword found, otherwise add the current to the process_word hashset.
+    
+        """
+        
+        wordList_set = set(wordList)
+        
+        if endWord not in wordList_set:
+            return 0
+        
+        visited = set()
+        
+        # BFS
+        Q = deque()
+        Q.append((beginWord, 1))
+        
+        # [hit, hot, dot, dog, log]
+        
+        # hit => hot => dot => dog => cog
+        
+        # hit => ait
+        # [h, i, t] => [a, i, t] => "ait"
         
         while Q:
             
-            for _ in range(len(Q)):
-                cur_word = Q.popleft() # 'hit'
-                
-                for j in range(len(cur_word)):
-                    cur_word_list = list(cur_word)
+            word, level = Q.popleft()
+            
+            
+            for i in range(len(word)):
+                new_word = list(word)
+                for j in range(26):
+                    new_word[i] = chr(ord('a') + j)
+                    new_word_str = "".join(new_word)
                     
-                    for k in range(ord('a'), ord('z') + 1):
-                        cur_word_list[j] = chr(k)
-                        
-                        potential_word = "".join(cur_word_list)
-                        if potential_word == endWord:
-                            return changes + 1
-                        
-                        if potential_word in wordList and potential_word not in visited:
-                            Q.append(potential_word)
-                            visited.add(potential_word)
-            changes += 1
-        return 0
-                
-                
+                    if new_word_str == endWord:
+                        return level + 1
+                    
+                    if new_word_str not in visited and new_word_str in wordList_set:
+                        visited.add(new_word_str)
+                        Q.append((new_word_str, level + 1))
         
+        return 0
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            

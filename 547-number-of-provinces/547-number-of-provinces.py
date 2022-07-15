ea9d2742c6c18c5af0bf7f862f763b04d3eb0f01
Shiva Acharya <cva.acharya8@gmail.com>
2022-07-15 00:02:01 -0700

    Time: 270 ms (64.13%), Space: 14.3 MB (77.70%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,36 +1,36 @@
 class UnionFind:
     def __init__(self, size):
         self.root = [i for i in range(size)]
-        self.rank = [1] * size  # Rank for union by rank optimization
-        self.count = size
+        #self.rank = [1] * size  # Rank for union by rank optimization
+        #self.count = size
     
     # Find optimization with path compression
     def find(self, x):
-        # while x != self.root[x]:
-        #     x = self.root[x]
-        #return x  
-        if x == self.root[x]:
-            return x
+        while x != self.root[x]:
+            x = self.root[x]
+        return x  
+#         if x == self.root[x]:
+#             return x
         
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
+#         self.root[x] = self.find(self.root[x])
+#         return self.root[x]
     
     # Union optimization by rank
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
-#         if rootX != rootY:
-#             self.root[rootY] = rootX
-        
         if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
-            else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
-            self.count -= 1
+            self.root[rootY] = rootX
+        
+        # if rootX != rootY:
+        #     if self.rank[rootX] > self.rank[rootY]:
+        #         self.root[rootY] = rootX
+        #     elif self.rank[rootX] < self.rank[rootY]:
+        #         self.root[rootX] = rootY
+        #     else:
+        #         self.root[rootY] = rootX
+        #         self.rank[rootX] += 1
+        #     self.count -= 1
     
     
     def getCount(self):
@@ -47,10 +47,12 @@ class Solution:
             for y in range(x+1, n):
                 if isConnected[x][y]:
                     disjoint_set.union(x, y)
-    
         
-        return disjoint_set.getCount()
-            
+        provinces = 0
+        for i in range(len(disjoint_set.root)):
+            if i == disjoint_set.root[i]:
+                provinces += 1
+        return provinces
         
         
         

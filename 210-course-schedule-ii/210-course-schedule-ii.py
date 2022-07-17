0b58dcf2a31173751b9b7d0464f6515827b1d50b
Shiva Acharya <cva.acharya8@gmail.com>
2022-07-17 11:39:58 -0700

    Time: 181 ms (34.45%), Space: 15.5 MB (88.69%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # Topological Sort
+        
+        # Edge Case
+        if not prerequisites:
+            return [i for i in range(numCourses)]
+        
+        # Build Graph and indegrees
+        G = defaultdict(list)
+        indegrees = defaultdict(int)
+        
+        for u, v in prerequisites:
+            G[v].append(u)
+            indegrees[u] += 1
+            
+        Q = deque()
+        
+        # Populate Q with 0 indegree nodes
+        for i in range(numCourses):
+            if i not in indegrees:
+                Q.append(i)
+        
+        res = []
+        
+        while Q:
+            course = Q.popleft()
+            res.append(course)
+            
+            for nei in G[course]:
+                indegrees[nei] -= 1
+                if indegrees[nei] == 0:
+                    Q.append(nei)
+        
+        return res if len(res) == numCourses else []
+            
+        

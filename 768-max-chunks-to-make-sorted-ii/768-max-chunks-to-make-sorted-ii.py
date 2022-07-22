6ae7a412cd326c3c7df94cfee0d715c3249fcc51
Shiva Acharya <cva.acharya8@gmail.com>
2022-07-21 20:46:42 -0700

    Time: 87 ms (84.47%), Space: 14.2 MB (80.26%) - LeetHub

--- 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.py
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.py
@@ -1,19 +1,22 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
+        
         min_array = [0] * len(arr)
         min_array[-1] = arr[-1]
+        
+        # Caluculate running min from the end of arr
+        for i in range(len(arr) - 2, -1, -1):
+            min_array[i] = min(min_array[i+1], arr[i])
+        
         chunks = 1
         
-        # Calculate running min from the end
-        for i in range(len(arr) - 2, -1, -1):
-            min_array[i] = min(arr[i], min_array[i+1])
-        
-        # Calculate max_so_far from the front and chunks
-        max_so_far = arr[0]
+        # Calculate max_so_far from the front and update chunks if needed
+        max_so_far = 0
         for i in range(len(arr) - 1):
             max_so_far = max(max_so_far, arr[i])
-            if max_so_far <= min_array[i + 1]:
+            
+            if max_so_far <= min_array[i+1]:
                 chunks += 1
-
         
         return chunks
+        

9c950b104af031d91f920b1433e2a665396d01eb
Shiva Acharya <cva.acharya8@gmail.com>
2022-07-25 22:12:08 -0700

    Time: 377 ms (64.10%), Space: 19.7 MB (42.12%) - LeetHub

---
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -0,0 +1,31 @@
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+
+        # Create a graph
+        G = defaultdict(list)
+        for u, v in edges:
+
+            G[u].append(v)                
+        
+        visited = set()
+        def dfs(s):
+            
+            # Check if s is end node and not destination
+            if not G[s] and s != destination:
+                return False
+            
+            # Check for cycle
+            if s in visited:
+                return False
+            
+            visited.add(s)
+            
+            for node in G[s]:
+                if not dfs(node):
+                    return False
+                visited.remove(node)
+            
+            return True
+            
+        
+        return dfs(source)

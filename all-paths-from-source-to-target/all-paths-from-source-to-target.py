78198491c7620c53c5c69db6e158fe4990be8eec
Shiva Acharya <cva.acharya8@gmail.com>
2022-08-13 23:46:06 -0700

    Time: 184 ms, Memory: 15.7 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -11,7 +11,7 @@ class Solution:
             
             if node == destination:
                 res.append(path)
-                continue
+                #continue
             
             for nei in graph[node]:
                 Q.append(path + [nei])

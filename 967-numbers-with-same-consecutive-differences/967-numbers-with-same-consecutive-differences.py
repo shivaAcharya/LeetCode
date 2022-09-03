70addb514456ad1682bf5e6e574a876a22912e4b
Shiva Acharya <cva.acharya8@gmail.com>
2022-09-03 14:47:31 -0700

    Time: 72 ms (40.20%), Space: 14.3 MB (41.53%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -3,21 +3,20 @@ class Solution:
         res = []
         tmp = [] # ['1', '2', '4']
         
-        def backtrack(i):
+        def backtrack():
             if len(tmp) == n:
                 res.append(int("".join(tmp)))
                 return
             
             for nei in range(0, 10):
-                if i == 0 and nei == 0:
+                if not tmp and nei == 0:
                     continue
                 
                 if not tmp or abs(int(tmp[-1]) - nei) == k:
                     tmp.append(str(nei))
-                
-                    backtrack(i + 1)
+                    backtrack()
                     tmp.pop()
         
         
-        backtrack(0)
+        backtrack()
         return res

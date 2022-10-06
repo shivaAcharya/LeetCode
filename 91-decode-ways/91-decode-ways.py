670fb490f8b93361d76aa4033b333f42de3be2d1
Shiva Acharya <cva.acharya8@gmail.com>
2022-10-05 22:42:10 -0700

    Time: 65 ms (32.48%), Space: 14 MB (46.30%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,30 +1,27 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         
-        dp = [0] * (len(s) + 1)
+        N = len(s)
+        dp = [0] * (N + 1)
         dp[-1] = 1
         
-        for i in range(len(s) - 1, -1, -1):
+        for i in range(N - 1, -1, -1):
+            
             if s[i] == '0':
                 dp[i] = 0
-            elif s[i] == '1' and i + 1 < len(s):
-                dp[i] = dp[i+1] + dp[i+2]
-                
-            elif s[i] == '2':
-                dp[i] = dp[i+1]
-                
-                if i + 1 < len(s) and s[i+1] <= '6':
+            
+            elif s[i] == '1':
+                dp[i] += dp[i+1]
+                if i + 1 < N:
                     dp[i] += dp[i+2]
+            
+            elif s[i] == '2':
+                dp[i] += dp[i+1]
+                if i + 1 < N and s[i+1] <= '6':
+                    dp[i] += dp[i+2]
+            
             else:
                 dp[i] = dp[i+1]
         
         return dp[0]
-
-    
-"""
-FAILED TESTCASES
-
-1. "1"
-2. "12"
-
-"""
+        

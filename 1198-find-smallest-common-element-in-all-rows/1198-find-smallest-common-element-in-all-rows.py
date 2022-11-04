fc2a3408db8359deaf0026d9b3116ea5c46f4bf8
Shiva Acharya <cva.acharya8@gmail.com>
2022-11-03 17:51:38 -0700

    Time: 590 ms (84.13%), Space: 39 MB (94.35%) - LeetHub

--- 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.py
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.py
@@ -1,5 +1,24 @@
 class Solution:
     def smallestCommonElement(self, mat: List[List[int]]) -> int:
+        '''
+        1. Since the elements are stricty increasing => No duplicates
+        2. Count the frequency of every element and return the first element with the frequency equal to the number of rows.
+        
+        '''
+        ROWS, COLS = len(mat), len(mat[0])
+        frequency = {}
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                num = mat[r][c]
+                frequency[num] = frequency.get(num, 0) + 1
+                if frequency[num] == ROWS:
+                    return num
+        return -1
+        
+        '''
+        ################### SIMULATION APPROACH TIME COMPLEXITY => O(M^2N^2)
+        
         ROWS = len(mat)
         
         def check(row, num):
@@ -18,3 +37,4 @@ class Solution:
                     return num
         
         return -1
+        '''

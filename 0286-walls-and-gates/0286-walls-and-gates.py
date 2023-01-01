75158c3e554936418a5582b21a62bfd51db5bb67
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-01 14:24:57 -0800

    Time: 257 ms (92.12%), Space: 16.8 MB (91.84%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -12,20 +12,13 @@ class Solution:
             for c in range(COLS):
                 if rooms[r][c] == 0:
                     Q.append((r, c))
-        
-        level = 1
-        Q.append((-1, -1))
+  
         # BFS from all gates
         while Q:
-            
             row, col = Q.popleft()
-            if (row, col) == (-1, -1):
-                level += 1
-                if Q:
-                    Q.append((-1, -1))
-                continue
+
             for r, c in (row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1):
                 if 0 <= r < ROWS and 0 <= c < COLS and rooms[r][c] == 2147483647:
-                    rooms[r][c] = level
+                    rooms[r][c] = rooms[row][col] + 1
                     Q.append((r, c))
                     

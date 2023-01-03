9ca95b8a0af8468d146fd4fe88eb6a9c501ed9d7
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-02 21:04:31 -0800

    Time: 40 ms (79.14%), Space: 20.5 MB (8.31%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -4,25 +4,11 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        '''
-        1. Can be empty linked list.
-        2. If only one node, return the node
+    def reverseList(self, head: Optional[ListNode], prev=None) -> Optional[ListNode]:
+        if not head: return prev
         
+        nxt = head.next
+        head.next = prev
         
-        Algorithm
-        1. Use prev, cur, and nxt pointers.
-        2. While cur and cur.next:
-        3.  nxt = cur.next => cur.next = prev => prev = cur => cur = nxt
-        4. Return prev
-                
-        '''
-        cur, prev = head, None
+        return self.reverseList(nxt, head)
         
-        while cur:
-            nxt = cur.next
-            cur.next = prev
-            prev = cur
-            cur = nxt
-        
-        return prev

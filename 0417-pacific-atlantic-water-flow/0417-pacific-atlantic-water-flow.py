2531de30a382fc22ef6493fa5d6097ec428c5bcb
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-11 19:41:34 -0800

    Time: 256 ms (99.68%), Space: 15.5 MB (95.03%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,48 +1,48 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        
+        P_Q, A_Q, P_S, A_S = deque(), deque(), set(), set()
         ROWS, COLS = len(heights), len(heights[0])
-        pacific_Q, atlantic_Q = deque(), deque()
-        pacific_set, atlantic_set = set(), set()
 
-        # Populate Qs
-        # Add top row
+        # Populate P_Q and P_S
         for c in range(COLS):
-            pacific_Q.append((0, c))
-        
-        # Add first col
-        for r in range(ROWS):
-            pacific_Q.append((r, 0))
+            P_Q.append((0, c))
+            P_S.add((0, c))
 
-        # Add last row
+        for r in range(ROWS):
+            P_Q.append((r, 0))
+            P_S.add((r, 0))
+        
+        # Populate A_Q and A_S
         for c in range(COLS):
-            atlantic_Q.append((ROWS - 1, c))
+            A_Q.append((ROWS - 1, c))
+            A_S.add((ROWS - 1, c))
         
-        # Add last col
         for r in range(ROWS):
-            atlantic_Q.append((r, COLS - 1))
+            A_Q.append((r, COLS - 1))
+            A_S.add((r, COLS - 1))
+
+        # BFS on P_Q
+        while P_Q:
+            r, c = P_Q.popleft()
+
+            for row, col in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
+                if 0 <= row < ROWS and 0 <= col < COLS and (row, col) not in P_S and heights[row][col] >= heights[r][c]:
+                    P_S.add((row, col))
+                    P_Q.append((row, col))
+
+        # BFS on A_Q
+        while A_Q:
+            r, c = A_Q.popleft()
+
+            for row, col in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
+                if 0 <= row < ROWS and 0 <= col < COLS and (row, col) not in A_S and heights[row][col] >= heights[r][c]:
+                    A_S.add((row, col))
+                    A_Q.append((row, col))
         
-        while pacific_Q:
-            row, col = pacific_Q.popleft()
-
-            pacific_set.add((row, col))
-
-            for r, c in (row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1):
-                if 0 <= r < ROWS and 0 <= c < COLS and (r, c) not in pacific_set and heights[r][c] >= heights[row][col]:
-                    pacific_Q.append((r, c))
-        
-        while atlantic_Q:
-            row, col = atlantic_Q.popleft()
-
-            atlantic_set.add((row, col))
-
-            for r, c in (row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1):
-                if 0 <= r < ROWS and 0 <= c < COLS and (r, c) not in atlantic_set and heights[r][c] >= heights[row][col]:
-                    atlantic_Q.append((r, c))
-        
+        # Calculate result
         res = []
-        for (r, c) in pacific_set:
-            if (r, c) in atlantic_set:
+        for r, c in P_S:
+            if (r, c) in A_S:
                 res.append([r, c])
         
-        return res
+        return res 

9000bd2f57578ebac9740c0b2ea3cc7fe0362403
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-16 18:59:19 -0800

    Time: 31 ms (93.63%), Space: 13.9 MB (25.59%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,3 +1,25 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1) * int(num2))
+        
+        def to_int(s):
+            res = 0
+            for n in s:
+                res = res * 10 + ord(n) - ord('0')
+            
+            return res
+        
+        def to_str(i):
+            if not i: return '0'
+            
+            res = []
+            while i:
+                n = chr(i % 10 + ord('0'))
+                res.append(n)
+                i //= 10
+            
+            return "".join(reversed(res))
+        
+        res_int = to_int(num1) * to_int(num2)
+        
+        return to_str(res_int)
+            

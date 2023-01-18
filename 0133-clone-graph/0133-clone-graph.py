32abb44a0ecc4d53904904f90dd522b56d51a7b2
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-17 19:58:54 -0800

    Time: 43 ms (67.46%), Space: 14.4 MB (23.99%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -11,17 +11,18 @@ class Solution:
         old_to_new = {}
 
         def dfs(node):
-
             if not node: return
+
             if node in old_to_new: return old_to_new[node]
 
             # Make a copy
             copy_node = Node(node.val)
             old_to_new[node] = copy_node
 
+            # Traverse neigbors
             for nei in node.neighbors:
                 copy_node.neighbors.append(dfs(nei))
 
             return copy_node
-
+        
         return dfs(node)

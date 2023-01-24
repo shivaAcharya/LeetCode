ec53fb3bc07e6b711dd1990426c0676ad359a348
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-23 22:58:11 -0800

    Time: 53 ms (36.20%), Space: 14.4 MB (72.72%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -4,25 +4,33 @@ class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
+
+1. Initialize a old_to_new hashmap.
+2. Perform DFS
+    Make a copy of node and store in the hashmap
+    return node
+3. Return dfs(node)
+
 """
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
+        
         old_to_new = {}
 
         def dfs(node):
             if not node: return
 
-            if node in old_to_new: return old_to_new[node]
-
-            # Make a copy
+            if node in old_to_new:
+                return old_to_new[node]
+            
+            # Make copy
             copy_node = Node(node.val)
             old_to_new[node] = copy_node
 
-            # Traverse neigbors
             for nei in node.neighbors:
                 copy_node.neighbors.append(dfs(nei))
-
+            
             return copy_node
         
         return dfs(node)

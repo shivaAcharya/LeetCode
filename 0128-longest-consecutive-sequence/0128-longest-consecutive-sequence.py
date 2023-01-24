3da80875d957a64ee4ee57c8701460eed43bd98d
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-23 19:11:20 -0800

    Time: 5867 ms (7.59%), Space: 29 MB (60.02%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,23 +1,27 @@
-'''
-1. Create a nums_set from nums for O(1) lookups
-2. Find the smallest element and calculate cur_sequence.
-3. Maitain lcs
-4. Return lcs
-'''
+"""
+1. Convert nums to nums_set for O(1) lookups
+2. Look for the smallest num
+3.  Increment count for every num + 1 and maintain max_count.
+4. Return max_count
+"""
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-
+        
         nums_set = set(nums)
-        lcs = 0
+        max_count = 0
 
         for num in nums:
-            count = 1
             if num - 1 not in nums_set:
+                count = 1
                 while num + 1 in nums_set:
                     count += 1
                     num += 1
-            
-            lcs = max(lcs, count)
+                max_count = max(max_count, count)
         
-        return lcs
-        
+        return max_count
+    
+"""
+Time => O(N)
+Space => O(N)
+"""

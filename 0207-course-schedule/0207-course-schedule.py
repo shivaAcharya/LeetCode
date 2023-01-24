8ebd380eb46cf3c05c8f8e062edde13b0262419c
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-23 22:28:29 -0800

    Time: 89 ms (98.78%), Space: 15.5 MB (75.50%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,23 +1,40 @@
+"""
+Topological Sort
+1. indegrees array = [0] * numCourses
+2. Build G and maintain indegrees from prerequisites
+    for u, v in prerequisites:
+        G = {
+            v : {u}
+        }
+        indegrees[u] += 1
+3. Initialize course_taken to 0 and Q with 0 indegree courses
+4. Perform BFS and maintain course_taken
+5. Return course_taken == numCourses
+
+"""
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
+        indegrees = [0] * numCourses
         G = defaultdict(set)
 
-        for a, b in prerequisites:
-            G[b].add(a)
-            indegree[a] += 1
+        for u, v in prerequisites:
+            G[v].add(u)
+            indegrees[u] += 1
         
-        Q = deque([course for course, indegree in enumerate(indegree) if indegree == 0])
-        
-        #print(indegree, G)
-        courses_taken = 0
+        Q = deque([course for course, indegree in enumerate(indegrees) if indegree == 0])
+
+        course_taken = 0
         while Q:
             course = Q.popleft()
-            courses_taken += 1
+            course_taken += 1
 
-            for nxt_course in G[course]:
-                indegree[nxt_course] -= 1
-                if indegree[nxt_course] == 0:
-                    Q.append(nxt_course)
+            for next_course in G[course]:
+                indegrees[next_course] -= 1
+                if indegrees[next_course] == 0:
+                    Q.append(next_course)
         
-        return courses_taken == numCourses
+        return course_taken == numCourses
+"""
+Time => O(V + E)  V => numCourses, E => len(prerequisites)
+Space => O(V + E)
+"""

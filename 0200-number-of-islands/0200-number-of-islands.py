4cc4716cd6d4538c2eb1017d1b4d39090d2fa379
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-23 21:46:04 -0800

    Time: 664 ms (28.80%), Space: 16.5 MB (64.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,19 +1,28 @@
+"""
+Perform DFS, Mark Cell Visited, Count Islands
+"""
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        
         ROWS, COLS = len(grid), len(grid[0])
 
         def dfs(r, c):
-            
-            grid[r][c] = '0'
+
+            grid[r][c] = "0"
 
             for row, col in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
-                if 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] == '1':
+                if 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] == "1":
                     dfs(row, col)
-
+        
         islands = 0
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == '1':
-                    dfs(r, c)
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1":
+                    dfs(i, j)
                     islands += 1
+        
         return islands
+"""
+Time => O(MN)
+Space => O(MN)
+"""

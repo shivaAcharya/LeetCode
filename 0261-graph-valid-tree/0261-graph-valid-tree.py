22532ca380dcc40c1c343d14cd7e2a4de483e8a7
Shiva Acharya <cva.acharya8@gmail.com>
2023-01-25 22:11:31 -0800

    Time: 192 ms (31.36%), Space: 16.2 MB (36.46%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,26 +1,36 @@
+"""
+1. Return False if len(edges) != n - 1
+2. Build Graph.
+3. Check for cycle Maintaining visited set.
+4. Return len(visited) == n
+
+"""
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) != n - 1: return False
         
+        if len(edges) != n - 1:
+            return False
+
         # Build Graph
         G = defaultdict(set)
-
         for u, v in edges:
             G[u].add(v)
             G[v].add(u)
         
-        # Initialize Q with 0
-        Q = deque([0])
-        visited = set([0])
 
-        while Q:
-            node = Q.popleft()
-            
+        def dfs(node):
+
             for nei in G[node]:
                 if nei not in visited:
                     visited.add(nei)
-                    Q.append(nei)
+                    dfs(nei)
         
+        visited = set([0])
+        dfs(0)
+
         return len(visited) == n
-        
-        
+
+"""
+Time => O(V + E)
+Space => O(V + E)
+"""

1900d54184de78ff5fada354e46ca4f9a69fee82
Shiva Acharya <cva.acharya8@gmail.com>
2023-02-03 18:43:52 -0800

    Time: 67 ms (86.69%), Space: 14.1 MB (28.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,10 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_counter = Counter(s1)
-        s1_length = len(s1)
+        # Fixed size sliding window
         
-        for i in range(len(s2)):
-            if s1_counter == Counter(s2[i : i + s1_length]):
+        need = Counter(s1)
+        have = {}
+        left = 0
+        for right, c in enumerate(s2):
+            have[c] = have.get(c, 0) + 1
+            if right < len(s1) - 1:
+                continue
+            #print(need, have)
+            if need == have:
                 return True
+            have[s2[left]] -= 1
+            if have[s2[left]] == 0:
+                del have[s2[left]]
+            left += 1
         
         return False

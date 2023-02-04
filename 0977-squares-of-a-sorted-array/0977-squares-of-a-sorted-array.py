4fb6d4387d7929326a8e41a4040fa152d483a248
Shiva Acharya <cva.acharya8@gmail.com>
2023-02-03 19:47:35 -0800

    Time: 239 ms (60.72%), Space: 16.3 MB (13.94%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,38 +1,16 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        # If all elements are either non-negative or non-positive, square each num and return them
-        res = []
-        if not nums: return res
-        if nums[0] >= 0: return [num * num for num in nums]
-        if nums[-1] <= 0: return [num * num for num in reversed(nums)]
-
-        L = len(nums)
-        left, right = 0, L - 1
-        # Find first non-negative num
-        for i, num in enumerate(nums):
-            if num >= 0:
-                left, right = i - 1, i
-                break
-
-        while left >= 0 or right < L:
-
-            if left < 0:
-                res.append(nums[right] ** 2)
-                right += 1
-                continue
-
-            if right > L - 1:
-                res.append(nums[left] ** 2)
-                left -= 1
-                continue 
-
-            left_num, right_num = nums[left], nums[right]
-
-            if left_num ** 2 < right_num ** 2:
-                res.append(left_num ** 2)
-                left -= 1
+        N = len(nums)
+        res = [0] * N
+        left, right = 0, N - 1
+        
+        for i in range(N - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
             else:
-                res.append(right_num ** 2)
-                right += 1
-
+                square = nums[left]
+                left += 1
+                
+            res[i] = square * square
         return res

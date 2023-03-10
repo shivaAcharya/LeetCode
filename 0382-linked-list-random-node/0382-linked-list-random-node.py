1f88b49945a9cf0a2accf64cee6bef03e74ccc98
Shiva Acharya <cva.acharya8@gmail.com>
2023-03-10 08:25:42 -0800

    Time: 67 ms (90.71%), Space: 17.2 MB (97.21%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.py
+++ 0382-linked-list-random-node/0382-linked-list-random-node.py
@@ -6,13 +6,20 @@
 class Solution:
 
     def __init__(self, head: Optional[ListNode]):
-        self.ll_values = []
-        while head:
-            self.ll_values.append(head.val)
-            head = head.next
+        self.head = head
+        self.length = 0
+        # Calculate length
+        cur = self.head
+        while cur:
+            self.length += 1
+            cur = cur.next
 
     def getRandom(self) -> int:
-        return random.choice(self.ll_values)
+        random_num = random.randrange(1, self.length + 1)
+        cur = self.head
+        for _ in range(random_num - 1):
+            cur = cur.next
+        return cur.val
 
 
 # Your Solution object will be instantiated and called as such:

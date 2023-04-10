3192d7111405ff5fc6be4a55a0b8a58e8e52e30b
Shiva Acharya <cva.acharya8@gmail.com>
2023-04-09 19:58:12 -0700

    Time: 66 ms (54.90%), Space: 42.9 MB (28.52%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,22 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    const openToCloseMap = {
+    const map = {
         '(' : ')',
         '{' : '}',
         '[' : ']'
     };
+    let stack = [];
     
-    var stack = [];
-    
-    for (let para of s){
-        if (para in openToCloseMap){
-            stack.push(openToCloseMap[para]);
-        } else if((stack.length == 0) || (stack.pop() != para)){
+    for (para of s){
+        if (para in map){
+            stack.push(map[para]);
+        } else if (stack.length == 0 || stack.pop() != para){
             return false;
         }
+        
     }
     
     return stack.length == 0;
-    
 };

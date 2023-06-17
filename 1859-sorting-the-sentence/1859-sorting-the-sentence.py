9759c0f71d9275b78be03ae778ac1332122dcc93
Shiva Acharya <cva.acharya8@gmail.com>
2023-06-17 08:40:01 -0700

    Time: 48 ms (34.78%), Space: 16.4 MB (22.56%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        # Split s with space delimiter
+        s = s.split()
+        res = [''] * len(s)
+        
+        for word in s:
+            res[int(word[-1]) - 1] = word[:len(word) - 1]
+        
+        return " ".join(res)

1b6f183cff6434db2f050d7895776b6f18e4996a
Shiva Acharya <cva.acharya8@gmail.com>
2023-10-08 11:31:16 -0700

    Time: 34 ms (83.47%), Space: 16.2 MB (70.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,28 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # 1. Creating an open to close bracket map.
-        # 2. Initialize an empty stack.
-        # 3. For every para in s:
-        # 4.    # If para == open => stack.push(map[para])
-        # 5.    # Else:
-        # 6.        If not stack: return False
-        # 7.        If stack.pop() != para: return False
-        # 8. return not stack
-        
-        open_to_close = {
+        para = {
             '(' : ')',
             '{' : '}',
             '[' : ']',
         }
-        
         stack = []
         
-        for para in s:
-            if para in open_to_close:
-                stack.append(open_to_close[para])
-            elif not stack:
-                return False
-            elif stack.pop() != para:
+        for p in s:
+            if p in para:
+                stack.append(para[p])
+            elif not stack or stack.pop() != p:
                 return False
         
         return not stack
+        
+        

3dfca1ff43642802f186b061813bf0c4220d2a51
Shiva Acharya <cva.acharya8@gmail.com>
2023-10-14 21:16:35 -0700

    Time: 1974 ms (26.89%), Space: 20.2 MB (90.20%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,55 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        '''
+        1. Sort nums.
+        2. For every num in nums:
+        3.  # Two pointer technique to find sum = 0
+        4.  # Skip duplicates
+        
+        '''
+        
         nums.sort()
-        res = []
         L = len(nums)
-
+        res = []
+        
         for i, num in enumerate(nums):
-            if i > 0 and num == nums[i-1]:
+
+            # Check for duplicates
+            if i > 0 and nums[i - 1] == num:
                 continue
             
+            # Two pointer
             l, r = i + 1, L - 1
+            
             while l < r:
-                cur_sum = nums[l] + nums[r] + num
+                # Skip duplicates
+                if l > i + 1 and nums[l] == nums[l - 1]:
+                    l += 1
+                    continue
+                if r < L - 1 and nums[r] == nums[r + 1]:
+                    r -= 1
+                    continue
+                
+                cur_sum = num + nums[l] + nums[r]
+                
+                # Check for 0 sum
                 if cur_sum == 0:
                     res.append([num, nums[l], nums[r]])
-
-                if nums[l] + nums[r] + num > 0:
-                    r -= 1
-                    while l < r and nums[r] == nums[r+1]:
-                        r -= 1
-                else:
                     l += 1
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
-
+                
+                # Move pointer based on sum
+                if cur_sum < 0:
+                    l += 1
+                else:
+                    r -= 1
+        
         return res
-"""
-nums = [-4, -1, -1, 0, 1, 2]
-
-"""
+        
+        '''
+        [-4, -1, -1, 0, 1, 2]
+        
+        [-1,-1,0,1]
+        L = 4
+        
+        '''
+        

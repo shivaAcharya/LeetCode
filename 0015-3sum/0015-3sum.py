2c32506603dcfea07f1f6c819b8a3fd054f53033
Shiva Acharya <cva.acharya8@gmail.com>
2023-10-17 19:03:00 -0700

    Time: 1194 ms (42.06%), Space: 20.3 MB (75.11%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,55 +1,38 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         '''
-        1. Sort nums.
-        2. For every num in nums:
-        3.  # Two pointer technique to find sum = 0
-        4.  # Skip duplicates
+        sorting, two pointer, skipping duplicates
+        
+        1. Sort nums
+        2. Use two pointer technique to find sum to be 0
+        3.      Skip duplicates when occur
         
         '''
         
         nums.sort()
-        L = len(nums)
         res = []
         
         for i, num in enumerate(nums):
-
-            # Check for duplicates
+            
             if i > 0 and nums[i - 1] == num:
                 continue
-            
-            # Two pointer
-            l, r = i + 1, L - 1
+                
+            # Two pointer technique
+            l, r = i + 1, len(nums) - 1
             
             while l < r:
-                # Skip duplicates
-                if l > i + 1 and nums[l] == nums[l - 1]:
-                    l += 1
-                    continue
-                if r < L - 1 and nums[r] == nums[r + 1]:
-                    r -= 1
-                    continue
                 
                 cur_sum = num + nums[l] + nums[r]
                 
-                # Check for 0 sum
                 if cur_sum == 0:
                     res.append([num, nums[l], nums[r]])
-                    l += 1
-                
-                # Move pointer based on sum
-                if cur_sum < 0:
-                    l += 1
-                else:
+                    
+                if cur_sum > 0:
                     r -= 1
+                else:
+                    l += 1
+                    # discard duplicates
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
         
         return res
-        
-        '''
-        [-4, -1, -1, 0, 1, 2]
-        
-        [-1,-1,0,1]
-        L = 4
-        
-        '''
-        

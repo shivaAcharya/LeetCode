557640d72b7735dfa01013e5d0e56123de827656
Shiva Acharya <cva.acharya8@gmail.com>
2023-10-18 17:45:57 -0700

    Time: 40 ms (48.44%), Space: 16.4 MB (20.69%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,33 +1,19 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
+        s_stack, t_stack = [], []
         
-        s_ptr, t_ptr = len(s) - 1, len(t) - 1
+        for letter in s:
+            if letter == '#':
+                if s_stack:
+                    s_stack.pop()
+            else:
+                s_stack.append(letter)
         
-        while s_ptr >= 0 or t_ptr >= 0:
-                       
-            back_str = 0
-            while s_ptr >= 0 and (s[s_ptr] == '#' or back_str):
-                if s[s_ptr] == '#':
-                    back_str += 1
-                else:
-                    back_str -= 1
-                s_ptr -= 1
-            
-            
-            back_str = 0
-            while t_ptr >= 0 and (t[t_ptr] == '#' or back_str):
-                if t[t_ptr] == '#':
-                    back_str += 1
-                else:
-                    back_str -= 1
-                t_ptr -= 1
-                
-            
-            if s_ptr >= 0 and t_ptr >= 0:
-                if s[s_ptr] != t[t_ptr]:
-                    return False
-                
-            s_ptr -= 1
-            t_ptr -= 1
+        for letter in t:
+            if letter == '#':
+                if t_stack:
+                    t_stack.pop()
+            else:
+                t_stack.append(letter)
         
-        return s_ptr == t_ptr
+        return s_stack == t_stack

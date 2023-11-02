896b16e0c7cfa2d2093fc2e0716574b7e03e57f2
Shiva Acharya <cva.acharya8@gmail.com>
2023-11-02 12:47:41 -0700

    Time: 40 ms (45.36%), Space: 16.1 MB (84.95%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -6,25 +6,26 @@ class Solution:
         
         while top < bottom and left < right:
             
-            # Go left to right
+            # Go from left to right
             for i in range(left, right):
                 res.append(matrix[top][i])
             top += 1
             
-            # Go top to bottom
+            # Go from top to bottom
             for i in range(top, bottom):
                 res.append(matrix[i][right - 1])
             right -= 1
             
-            if top >= bottom or left >= right:
+            # Check if the bounds are still valid
+            if not (top < bottom and left < right):
                 break
             
-            # Go right to left
+            # Go from right to left
             for i in range(right - 1, left - 1, -1):
                 res.append(matrix[bottom - 1][i])
             bottom -= 1
             
-            # Go bottom to top
+            # Go from bottom to top
             for i in range(bottom - 1, top - 1, -1):
                 res.append(matrix[i][left])
             left += 1

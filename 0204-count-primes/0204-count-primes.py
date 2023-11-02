4b88cb031ee5b76defc4fec03c93317efac810e8
Shiva Acharya <cva.acharya8@gmail.com>
2023-11-02 11:02:33 -0700

    Time: 2954 ms (36.59%), Space: 94.2 MB (24.62%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        
+        primes = 0
+        is_prime = [False, False] + [True] * (n - 1)
+        
+        for p in range(2, n):
+            if is_prime[p]:
+                primes += 1
+                # Seive out the multiples of p
+                for i in range(p, n, p):
+                    is_prime[i] = False
+        
+        return primes
+        

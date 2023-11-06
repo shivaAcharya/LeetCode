c0897725f6c91236d90a6c0d103997e2985b1cba
Shiva Acharya <cva.acharya8@gmail.com>
2023-11-05 21:40:00 -0800

    Time: 132 ms (34.34%), Space: 31.7 MB (39.42%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -8,21 +8,9 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         
         curA, curB = headA, headB
-        curA_switched = curB_switched = False
         
-        while curA and curB and curA != curB:
-            curA = curA.next
-            curB = curB.next
-            
-            if not curA and not curA_switched:
-                curA = headB
-                curA_switched = True
-            
-            if not curB and not curB_switched:
-                curB = headA
-                curB_switched = True
-        
-        if not curA or not curB:
-            return
-        
+        while curA != curB:
+            curA = curA.next if curA else headB
+            curB = curB.next if curB else headA
+                
         return curA

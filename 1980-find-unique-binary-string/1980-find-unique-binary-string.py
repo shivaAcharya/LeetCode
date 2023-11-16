2c050a4f45e1b6f5f9152217ef4a9f6fdf067277
Shiva Acharya <cva.acharya8@gmail.com>
2023-11-15 19:35:19 -0800

    Time: 39 ms (75.95%), Space: 16.6 MB (11.86%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        
+        """
+        Convert given array to set for constant time lookup.
+        Use backtracking to create all possible binary string of length n and 
+        check if the string exists in the set.
+        
+        """
+        
+        set_nums = set(nums)
+        res = None
+        binary_digits = []
+        
+        def backtrack(n):
+            nonlocal res
+            if res:
+                return
+            
+            if n == len(nums):
+                binary_str = "".join(binary_digits)
+                if binary_str not in set_nums:
+                    res = binary_str
+                return
+            
+            for next_dig in '01':
+                binary_digits.append(next_dig)
+                backtrack(n + 1)
+                binary_digits.pop()
+            
+        
+        backtrack(0)
+        
+        return res
+        

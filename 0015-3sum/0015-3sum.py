757b49790b54d530e1ed4991b5c047c5de195025
Shiva Acharya <cva.acharya8@gmail.com>
2023-11-29 17:03:51 -0800

    Time: 1501 ms (35.65%), Space: 20.5 MB (47.89%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,40 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        '''
-        sorting, two pointer, skipping duplicates
-        
-        1. Sort nums
-        2. Use two pointer technique to find sum to be 0
-        3.      Skip duplicates when occur
-        
-        '''
-        
         nums.sort()
         res = []
         
+        # [-4, -1, -1, -1, 0, 1, 2]
+        
         for i, num in enumerate(nums):
-            
-            if i > 0 and nums[i - 1] == num:
+            # Remove duplicates from the first pointer
+            if i > 0 and num == nums[i - 1]:
                 continue
-                
+            
             l, r = i + 1, len(nums) - 1
             
-            while l < r:            
+            # Use two pointers
+            while l < r:
                 cur_sum = num + nums[l] + nums[r]
                 
                 if cur_sum == 0:
                     res.append([num, nums[l], nums[r]])
-                    
-                if cur_sum < 0:
-                    l += 1
-                    # Skip duplicates
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-                else:
+                
+                if cur_sum > 0:
                     r -= 1
-                    # Skip duplicates
                     while l < r and nums[r] == nums[r + 1]:
                         r -= 1
+                else:
+                    l += 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
         
-        return res
-            
+        return res                
+        

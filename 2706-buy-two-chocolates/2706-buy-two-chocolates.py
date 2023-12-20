845eba2ac324f8c578c90ba3588594391dfad078
Shiva Acharya <cva.acharya8@gmail.com>
2023-12-20 10:19:27 -0800

    Time: 63 ms (65.97%), Space: 17.4 MB (23.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,8 +1,15 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         
-        prices.sort()
-        left_over = money - (prices[0] + prices[1])
+        first_min = second_min = 101
+        
+        for price in prices:
+            if price < first_min:
+                first_min, second_min = price, first_min
+            elif price < second_min:
+                second_min = price            
+        
+        left_over = money - (first_min + second_min)
         
         return left_over if left_over >= 0 else money
     

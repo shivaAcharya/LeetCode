1bbcfbf50bb5ebad36ce2880c91d864211adcbe3
Shiva Acharya <cva.acharya8@gmail.com>
2024-01-14 19:26:37 -0800

    Time: 4865 ms (5.02%), Space: 49.6 MB (33.13%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,12 +1,14 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        # If source and target is same no need for bus
+        # If source is target, no bus needed
         if source == target: return 0
         
-        routes_buses = defaultdict(set) # route -> bus
-        
-        Q = deque()
+        # Initialization
+        routes_buses = defaultdict(set) # routes -> buses
+        Q = deque() # (bus, bus_count)
         target_buses = set()
+        
+        # Build routes_buses
         for bus, route in enumerate(routes):
             for rt in route:
                 if rt == source:
@@ -14,29 +16,28 @@ class Solution:
                 if rt == target:
                     target_buses.add(bus)
                 routes_buses[rt].add(bus)
-        
-        # Build Graph
-        G = defaultdict(set) # Undirected buses
-        
+                
+        # Build undirected graph for buses
+        G = defaultdict(set)
         for bus, route in enumerate(routes):
             for rt in route:
                 buses = routes_buses[rt]
                 for b in buses:
                     G[bus].add(b)
                     G[b].add(bus)
-                
         
-        seen = set()
+        # BFS
+        seen = set() # To avoid cycle
         while Q:
             bus, bus_count = Q.popleft()
+            
             if bus in target_buses:
                 return bus_count
             
             seen.add(bus)
-            
-            for nei in G[bus]:
-                if nei not in seen:
-                    Q.append((nei, bus_count + 1))
+            for next_bus in G[bus]:
+                if next_bus not in seen:
+                    Q.append((next_bus, bus_count + 1))
         
         return -1
-                        
+        

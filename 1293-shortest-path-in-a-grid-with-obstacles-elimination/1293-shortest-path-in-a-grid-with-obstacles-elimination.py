9647f7297c988d8b07a3792c76ad99e07d5aa670
Shiva Acharya <cva.acharya8@gmail.com>
2024-01-15 17:27:54 -0800

    Time: 67 ms (91.49%), Space: 19 MB (63.67%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,36 +1,26 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        rows, cols = len(grid), len(grid[0])
-        target = (rows - 1, cols - 1)
-
+        ROWS, COLS = len(grid), len(grid[0])
+        Q = deque([(0, 0, k, 0)]) # (r, c, k, steps)
+        
         # if we have sufficient quotas to eliminate the obstacles in the worst case,
         # then the shortest distance is the Manhattan distance
-        if k >= rows + cols - 2:
-            return rows + cols - 2
-
-        # (row, col, remaining quota to eliminate obstacles)
-        state = (0, 0, k)
-        # (steps, state)
-        queue = deque([(0, state)])
-        seen = set([state])
-
-        while queue:
-            steps, (row, col, k) = queue.popleft()
-
-            # we reach the target here
-            if (row, col) == target:
+        if k >= ROWS + COLS - 2:
+            return ROWS + COLS - 2
+        
+        visited = set()
+        visited.add((0, 0, k)) # (r, c, remaining_obstacles)
+        while Q:
+            r, c, k, steps = Q.popleft()
+            
+            if (r, c) == (ROWS - 1, COLS - 1):
                 return steps
-
-            # explore the four directions in the next step
-            for new_row, new_col in [(row, col + 1), (row + 1, col), (row, col - 1), (row - 1, col)]:
-                # if (new_row, new_col) is within the grid boundaries
-                if (0 <= new_row < rows) and (0 <= new_col < cols):
-                    new_eliminations = k - grid[new_row][new_col]
-                    new_state = (new_row, new_col, new_eliminations)
-                    # add the next move in the queue if it qualifies
-                    if new_eliminations >= 0 and new_state not in seen:
-                        seen.add(new_state)
-                        queue.append((steps + 1, new_state))
-
-        # did not reach the target
+            
+            for row, col in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
+                if 0 <= row < ROWS and 0 <= col < COLS:
+                    new_k = k - grid[row][col]
+                    if new_k >= 0 and (row, col, new_k) not in visited:
+                        visited.add((row, col, new_k))
+                        Q.append((row, col, new_k, steps + 1))
         return -1
+    

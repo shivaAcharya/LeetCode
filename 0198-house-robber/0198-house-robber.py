2359d7ef68450398b6a81806a92b046b039a4d34
Shiva Acharya <cva.acharya8@gmail.com>
2024-01-21 14:05:12 -0800

    Time: 30 ms (94.74%), Space: 16.7 MB (56.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,33 +1,39 @@
-'''
-            / \
-           0   1 
-          /\  / \
-         2 3 3
+"""
+nums = [1, 2, 3, 1]
 
-0 -> 2 => 4
-0 -> 3 => 2
-1 -> 3 => 3
+   1        2
+3     1   1
+
+maximize money
+cannot rob two adjacent houses
+
+[2,7,9,3,1]
+            start
+         /         \ 
+        2           7
+    9      3    3       1
+1              
 
 
-'''
+"""
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
-        prev, last = nums[0], max(nums[0], nums[1])
+        if len(nums) <= 2:
+            return max(nums)
         
-        for i in range(2, len(nums)):
-            prev, last = last, max(nums[i] + prev, last)
-        
-        return last
-        
-#         memo = {}
-#         def dp(i):
-#             if i >= len(nums):
-#                 return 0
+        memo = {}
+        def dfs(i):
+            if i >= len(nums):
+                return 0
             
-#             if i not in memo:
-#                 memo[i] = max(nums[i] + dp(i + 2), dp(i + 1))
+            if i in memo:
+                return memo[i]
             
-#             return memo[i]
-        
-#         return dp(0)
+            left = dfs(i + 1)
+            right = dfs(i + 2) + nums[i]
+            memo[i] =  max(left, right)
+            return memo[i]
+                    
+        return max(dfs(0), dfs(1))
+    

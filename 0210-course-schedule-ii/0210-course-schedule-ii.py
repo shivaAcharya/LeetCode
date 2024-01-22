074fc024702b4fd298a086ff6488cc7fb9e289e0
Shiva Acharya <cva.acharya8@gmail.com>
2024-01-21 21:33:43 -0800

    Time: 76 ms (99.75%), Space: 18.4 MB (78.19%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,48 +1,50 @@
+"""
+a, b => first take b to take a
+
+numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+start => indegrees 0 => [0]
+
+G
+0 -> 1, 2
+2 -> 3
+3 -> 2
+
+[0, 1, 2, 3]
+
+Initialize indegrees array.
+Intialize Q with indegrees 0 nodes
+Create Graph from prerequisites
+Initialize res array
+While Q:
+    decrement indegrees of next courses
+    Add new course with indegrees 0 to Q.
+Return res array if length of res == numCourses, otherwise []
+
+
+"""
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        """
-        indegrees = [0, 0, 0, 2]
-        G = {
-            0 : {1, 2}
-            1 : {3}
-            2 : {3}
-        }
-        Q = [0] # Initialize with indegrees 0
-        res = []
-        seen = set()
-        
-        Q = [1, 2]
-        # BFS
-        # If cycle return []
-        # Add node to res
-        # Traverse nei of node, decrement indegrees, add to Q if indegree == 0
-        
-        """
-        
         indegrees = [0] * numCourses
         G = defaultdict(set)
-        
         for a, b in prerequisites:
-            indegrees[a] += 1
             G[b].add(a)
+            indegrees[a] += 1
         
         Q = deque()
-        
         for course, indegree in enumerate(indegrees):
             if indegree == 0:
                 Q.append(course)
-        
-        # print(indegrees, G, Q)
+                
         res = []
         while Q:
             course = Q.popleft()
             res.append(course)
-            
             for next_course in G[course]:
                 indegrees[next_course] -= 1
                 if indegrees[next_course] == 0:
                     Q.append(next_course)
-        
+                    
         return res if len(res) == numCourses else []
         

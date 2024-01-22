5f218e7607f26f2d94339b34041478294c3fa66e
Shiva Acharya <cva.acharya8@gmail.com>
2024-01-21 20:05:14 -0800

    Time: 5029 ms (5.02%), Space: 48.9 MB (40.33%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,43 +1,91 @@
+"""
+routes = [[1,2,7],[3,6,7]], source = 1, target = 6
+
+
+bus_stops
+
+0   =>  1 -> 2 -> 7
+1   =>  3 -> 6 -> 7
+
+
+stop_buses
+1 -> 0
+2 -> 1
+7 -> 0, 1
+3 -> 1
+6 -> 1
+
+visited_buses = {0}
+Q = [(1, 0, 0)] # stop, bus, bus_count
+    [(2, 1, 1), (7, 1, 1)]
+    
+    
+Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
+
+bus_stops = {
+    0 -> (7, 12),
+    1 -> (4, 5, 15),
+    2 -> (6),
+    3 -> (15, 19),
+    4 -> (9, 12, 13),
+}
+
+stop_buses = {
+    7 -> 0,
+    12 -> 0,
+    4 -> 1,
+    5 -> 1,
+    15 -> 1,3
+    6 -> 2,
+    19 -> 3,
+    9 -> 4,
+    13 -> 4,
+}
+buses_taken = {1}
+Q = [(1, 1), (3, 1)] #bus, bus_count
+    [(3, 2)]
+
+"""
+
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        # If source is target, no bus needed
+        
+        # Edge case
         if source == target: return 0
         
-        # Initialization
-        routes_buses = defaultdict(set) # routes -> buses
-        Q = deque() # (bus, bus_count)
+        stop_buses = defaultdict(set)
+        Q = deque() #(bus, bus_count)
+        visited_buses = set()
         target_buses = set()
-        
-        # Build routes_buses
-        for bus, route in enumerate(routes):
-            for rt in route:
-                if rt == source:
+        for bus, stops in enumerate(routes):
+            for stop in stops:
+                if stop == source:
                     Q.append((bus, 1))
-                if rt == target:
+                if stop == target:
                     target_buses.add(bus)
-                routes_buses[rt].add(bus)
-                
-        # Build undirected graph for buses
-        G = defaultdict(set)
-        for bus, route in enumerate(routes):
-            for rt in route:
-                buses = routes_buses[rt]
-                for b in buses:
-                    G[bus].add(b)
-                    G[b].add(bus)
+                stop_buses[stop].add(bus)
         
-        # BFS
-        seen = set() # To avoid cycle
-        while Q:
+        # Bus_buses connection
+        bus_buses = defaultdict(set)
+        
+        for bus, stops in enumerate(routes):
+            for stop in stops:
+                buses = stop_buses[stop]
+                for b in buses:
+                    bus_buses[b].add(bus)
+                    bus_buses[bus].add(b)
+        
+        while Q: #[(0, 1)]
             bus, bus_count = Q.popleft()
-            
             if bus in target_buses:
                 return bus_count
             
-            seen.add(bus)
-            for next_bus in G[bus]:
-                if next_bus not in seen:
+            # print(bus, bus_count)
+            visited_buses.add(bus)
+            
+            for next_bus in bus_buses[bus]:          
+                if next_bus not in visited_buses:
                     Q.append((next_bus, bus_count + 1))
-        
-        return -1
+    
+        return -1        
         

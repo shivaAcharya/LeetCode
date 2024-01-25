ea8af0875e5fedb6e4494761065e921c7a9203d8
Shiva Acharya <cva.acharya8@gmail.com>
2024-01-24 23:28:11 -0800

    Time: 90 ms (81.11%), Space: 18.4 MB (78.65%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,50 +1,45 @@
 """
-a, b => first take b to take a
+Prerequisite = [a, b], take b before a
 
-numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
-start => indegrees 0 => [0]
+Initialize a Graph, adjacency list of courses
+Initialize an indegree counter
+Perform Topological sorting
+Add all courses with 0 indegrees into a Q
+Initialize res to accumulate courses
+While Q
+    popleft Q, and add it to res
+    Traverse the graph and decrement the indegrees
+    If indegrees == 0, add it to the queue
 
-G
-0 -> 1, 2
-2 -> 3
-3 -> 2
-
-[0, 1, 2, 3]
-
-Initialize indegrees array.
-Intialize Q with indegrees 0 nodes
-Create Graph from prerequisites
-Initialize res array
-While Q:
-    decrement indegrees of next courses
-    Add new course with indegrees 0 to Q.
-Return res array if length of res == numCourses, otherwise []
-
+return res if len(res) == n else []
+    
 
 """
-
+from collections import Counter, deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        indegrees = [0] * numCourses
         G = defaultdict(set)
+        indegrees = Counter()
+        
         for a, b in prerequisites:
             G[b].add(a)
             indegrees[a] += 1
-        
+            
         Q = deque()
-        for course, indegree in enumerate(indegrees):
-            if indegree == 0:
-                Q.append(course)
+        for n in range(numCourses):
+            if n not in indegrees:
+                Q.append(n)
                 
         res = []
         while Q:
             course = Q.popleft()
             res.append(course)
+            
             for next_course in G[course]:
                 indegrees[next_course] -= 1
                 if indegrees[next_course] == 0:
                     Q.append(next_course)
-                    
+        
         return res if len(res) == numCourses else []
         

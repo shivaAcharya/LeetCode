a51cad93be0d9a6dac4236cf649c0cbe1a6c3932
Shiva Acharya <cva.acharya8@gmail.com>
2024-01-25 19:17:22 -0800

    Time: 1394 ms (16.23%), Space: 365.5 MB (5.10%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -2,27 +2,32 @@ class Solution:
     def canCross(self, stones: List[int]) -> bool:
         
         mark = {}
+        # Mark stones in the map to identify if such stone exists or not.
+        for i in range(len(stones)):
+            mark[stones[i]] = i
+        
+        # Initialize with -1s to denote they are not calculated
         dp = [[-1] * 2001 for _ in range(2001)]
         
         def solve(idx, prev_jump):
-            
+            # If reached the last stone, return True
             if idx == len(stones) - 1:
                 return True
             
+            # If this subproblem has already been calculated, return it.
             if dp[idx][prev_jump] != -1:
                 return dp[idx][prev_jump] == 1
             
             ans = False
+            # Iterate over three possibilities {k - 1, k, k + 1}
             for nxt_jump in (prev_jump - 1), (prev_jump), (prev_jump + 1):
                 if nxt_jump > 0 and stones[idx] + nxt_jump in mark:
                     nxt_idx = mark[stones[idx] + nxt_jump]
                     ans = ans or solve(nxt_idx, nxt_jump)
             
+            # Store the result to fetch later
             dp[idx][prev_jump] = 1 if ans else 0
-            return ans
-        
-        for i in range(len(stones)):
-            mark[stones[i]] = i           
+            return ans          
             
         return solve(0, 0)
     

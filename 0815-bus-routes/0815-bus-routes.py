ca3f55fd0b406b3177f365709fae98dad5f6c291
Shiva Acharya <cva.acharya8@gmail.com>
2024-01-28 17:13:42 -0800

    Time: 5049 ms (5.00%), Space: 48.9 MB (42.75%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,91 +1,76 @@
 """
-routes = [[1,2,7],[3,6,7]], source = 1, target = 6
-
-
-bus_stops
-
-0   =>  1 -> 2 -> 7
-1   =>  3 -> 6 -> 7
-
-
-stop_buses
-1 -> 0
-2 -> 1
-7 -> 0, 1
-3 -> 1
-6 -> 1
-
-visited_buses = {0}
-Q = [(1, 0, 0)] # stop, bus, bus_count
-    [(2, 1, 1), (7, 1, 1)]
-    
-    
-Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
-
-bus_stops = {
-    0 -> (7, 12),
-    1 -> (4, 5, 15),
-    2 -> (6),
-    3 -> (15, 19),
-    4 -> (9, 12, 13),
+route_bus = {
+    1 : 0,
+    2 : 0,
+    7 : 0, 1,
+    3 : 1,
+    6 : 1,
 }
 
-stop_buses = {
-    7 -> 0,
-    12 -> 0,
-    4 -> 1,
-    5 -> 1,
-    15 -> 1,3
-    6 -> 2,
-    19 -> 3,
-    9 -> 4,
-    13 -> 4,
+bus_buses = {
+    0 : 1,
+    1 : 0,
 }
-buses_taken = {1}
-Q = [(1, 1), (3, 1)] #bus, bus_count
-    [(3, 2)]
+
+Q = (source, bus_num, bus_count) # (1, 0, 1)
+
+
+route_bus = {
+    7 : 0, 4
+   12 : 0,
+    4 : 1,
+    5 : 1,
+   15 : 1,
+    6 : 2,
+   15 : 3,
+   19 : 3,
+    9 : 4,
+   13 : 4,
+}
+
 
 """
-
+from collections import deque, defaultdict
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         
-        # Edge case
-        if source == target: return 0
+        if source == target:
+            return 0
         
-        stop_buses = defaultdict(set)
-        Q = deque() #(bus, bus_count)
-        visited_buses = set()
+        Q = deque() #(bus_num, bus_count)
         target_buses = set()
-        for bus, stops in enumerate(routes):
-            for stop in stops:
-                if stop == source:
-                    Q.append((bus, 1))
-                if stop == target:
-                    target_buses.add(bus)
-                stop_buses[stop].add(bus)
+        route_bus = defaultdict(set)
         
-        # Bus_buses connection
+        for bus, route in enumerate(routes):
+            for rt in route:
+                route_bus[rt].add(bus)
+                if rt == source:
+                    Q.append((bus, 1))
+                if rt == target:
+                    target_buses.add(bus)
+        
         bus_buses = defaultdict(set)
         
-        for bus, stops in enumerate(routes):
-            for stop in stops:
-                buses = stop_buses[stop]
-                for b in buses:
+        for bus, route in enumerate(routes):
+            for rt in route:
+                for b in route_bus[rt]:
                     bus_buses[b].add(bus)
                     bus_buses[bus].add(b)
-        
-        while Q: #[(0, 1)]
+        # print(Q)
+        visited_bus = set()
+        while Q:
             bus, bus_count = Q.popleft()
+            
             if bus in target_buses:
                 return bus_count
             
-            # print(bus, bus_count)
-            visited_buses.add(bus)
+            visited_bus.add(bus)
             
-            for next_bus in bus_buses[bus]:          
-                if next_bus not in visited_buses:
-                    Q.append((next_bus, bus_count + 1))
-    
-        return -1        
+            for nxt_bus in bus_buses[bus]:
+                if nxt_bus not in visited_bus:
+                    Q.append((nxt_bus, bus_count + 1))
+                    
+        return -1
+                        
+            
         

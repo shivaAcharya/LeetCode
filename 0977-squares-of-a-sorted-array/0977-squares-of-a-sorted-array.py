fa9d3b49d12df5fc564d1234f536d388d52813e3
Shiva Acharya <cva.acharya8@gmail.com>
2024-03-02 14:57:39 -0800

    Time: 143 ms (96.73%), Space: 18.9 MB (54.67%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,19 +1,49 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        """
+        Find the pivot point
+        Use two pointer technique to create res
+        """
+              
+        r = 0
+        while r < len(nums):
+            if nums[r] >= 0:
+                break
+            r += 1
         
-        l, r = 0, len(nums) - 1
+        # For all negative numbers
+        if r == 0:
+            return [num * num for num in nums]
         
-        squares = [0] * len(nums)
-        write_idx = len(nums) - 1
-        
-        while l <= r:
-            if abs(nums[l]) < abs(nums[r]):
-                squares[write_idx] = nums[r] * nums[r]
-                r -= 1
+        # For all positive numbers
+        if r == len(nums):
+            return [num * num for num in reversed(nums)]
+            
+        l = r - 1
+        res = []
+        while l >= 0 or r < len(nums):
+            # print(f"l : {l}, r : {r}")
+            sqr1 = nums[l] * nums[l] if l >= 0 else None
+            sqr2 = nums[r] * nums[r] if r < len(nums) else None
+            # print(f"sqr1 : {sqr1}, sqr2 : {sqr2}")
+              
+            if sqr1 is not None and sqr2 is not None:
+                if sqr1 < sqr2:
+                    res.append(sqr1)
+                    l -= 1
+                else:
+                    res.append(sqr2)
+                    r += 1
+            elif sqr1:
+                res.append(sqr1)
+                l -= 1
             else:
-                squares[write_idx] = nums[l] * nums[l]
-                l += 1
-            write_idx -= 1
-        
-        return squares
+                res.append(sqr2)
+                r += 1
+            
+            # print(f"res : {res}")
+            # print(f"l : {l}, r : {r}")
+
+            
+        return res
     

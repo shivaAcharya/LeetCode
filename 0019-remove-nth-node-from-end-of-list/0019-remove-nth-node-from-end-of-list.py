17d2b20eaada5a4269d5718c439c1dda7d929c64
Shiva Acharya <cva.acharya8@gmail.com>
2024-03-02 10:06:40 -0800

    Time: 31 ms (90.39%), Space: 16.6 MB (62.65%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -8,17 +8,17 @@ class Solution:
         
         slow = fast = head
         
+        # Move fast pointer first
         for _ in range(n):
             fast = fast.next
         
         if not fast:
             return head.next
-            
-        while fast.next:
+        
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next
         
         slow.next = slow.next.next
-        
         return head
-    
+        

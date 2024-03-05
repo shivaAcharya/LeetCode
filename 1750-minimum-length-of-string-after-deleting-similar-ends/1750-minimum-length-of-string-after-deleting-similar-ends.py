eca9b643585836f55d679f3f4e5d4e00e862e041
Shiva Acharya <cva.acharya8@gmail.com>
2024-03-05 10:26:09 -0800

    Time: 80 ms (33.18%), Space: 17.4 MB (53.55%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -0,0 +1,29 @@
+"""
+s = "a a b c c a b b a"
+           l   r
+
+"""
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            
+            if s[l] != s[r]:
+                break
+            
+            # Move left pointer
+            l += 1
+            while l < r and s[l] == s[l - 1]:
+                l += 1
+                
+            # Move right pointer
+            r -= 1
+            while l < r and s[r] == s[r + 1]:
+                r -= 1
+            
+        
+        return r - l + 1
+        

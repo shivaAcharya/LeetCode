69ad23214a27a0f13b9fb5c8e5cf70e0800c402e
Shiva Acharya <cva.acharya8@gmail.com>
2024-04-12 10:29:54 -0700

    Time: 149 ms (63.25%), Space: 217 MB (77.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.rb
+++ 0049-group-anagrams/0049-group-anagrams.rb
@@ -1,13 +1,11 @@
 # @param {String[]} strs
 # @return {String[][]}
 def group_anagrams(strs)
-    groups = Hash.new { |hash, key| hash[key] = []}
+    groups = Hash.new {|hash, key| hash[key] = []}
     
     strs.each do |str|
         sorted_str = str.chars.sort.join
         groups[sorted_str].push(str)
     end
-    
     groups.values
-    
 end

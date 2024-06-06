4d26acbd23abebf39b60f6ad1c8a87163aa3ed02
Shiva Acharya <cva.acharya8@gmail.com>
2024-06-06 15:50:13 -0700

    Time: 161 ms (64.68%), Space: 18.4 MB (51.03%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,35 @@
+"""
+[1, 2, 2, 3, 3, 4, 6, 7, 8]
+
+
+https://leetcode.com/problems/hand-of-straights/solution/
+"""
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        hand_size = len(hand)
+
+        if hand_size % groupSize != 0:
+            return False
+
+        # Counter to store the count of each card value
+        card_count = Counter(hand)
+
+        # Min-heap to process the cards in sorted order
+        min_heap = list(card_count.keys())
+        heapq.heapify(min_heap)
+
+        # Process the cards until the heap is empty
+        while min_heap:
+            current_card = min_heap[0]  # Get the smallest card value
+            # Check each consecutive sequence of groupSize cards
+            for i in range(groupSize):
+                if card_count[current_card + i] == 0:
+                    return False
+                card_count[current_card + i] -= 1
+                if card_count[current_card + i] == 0:
+                    if current_card + i != heapq.heappop(min_heap):
+                        return False
+
+        return True
+    

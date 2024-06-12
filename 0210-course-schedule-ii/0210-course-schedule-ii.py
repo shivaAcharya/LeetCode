f9211905178b356d477de6a7df02aac6afbd9023
Shiva Acharya <cva.acharya8@gmail.com>
2024-06-12 08:42:28 -0700

    Time: 90 ms (59.87%), Space: 18.2 MB (79.55%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,21 +1,3 @@
-"""
-Prerequisite = [a, b], take b before a
-
-Initialize a Graph, adjacency list of courses
-Initialize an indegree counter
-Perform Topological sorting
-Add all courses with 0 indegrees into a Q
-Initialize res to accumulate courses
-While Q
-    popleft Q, and add it to res
-    Traverse the graph and decrement the indegrees
-    If indegrees == 0, add it to the queue
-
-return res if len(res) == n else []
-    
-
-"""
-from collections import Counter, deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
@@ -26,20 +8,15 @@ class Solution:
             G[b].add(a)
             indegrees[a] += 1
             
-        Q = deque()
-        for n in range(numCourses):
-            if n not in indegrees:
-                Q.append(n)
-                
+        Q = deque([course for course in range(numCourses) if course not in indegrees])
         res = []
+        
         while Q:
             course = Q.popleft()
             res.append(course)
-            
             for next_course in G[course]:
                 indegrees[next_course] -= 1
                 if indegrees[next_course] == 0:
                     Q.append(next_course)
-        
         return res if len(res) == numCourses else []
         

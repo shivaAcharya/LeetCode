c7572c0ca8a664e65ec4e279ce97bbe06aa85bfd
Shiva Acharya <cva.acharya8@gmail.com>
2024-06-14 14:01:31 -0700

    Time: 553 ms (97.61%), Space: 30.1 MB (89.78%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/minimum-increment-to-make-array-unique/solution/
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_val = max(nums)
+        min_increments = 0
+
+        # Create a frequencyCount array to store the frequency of each value in nums
+        frequency_count = [0] * (n + max_val + 1)
+
+        # Populate frequencyCount array with the frequency of each value in nums
+        for val in nums:
+            frequency_count[val] += 1
+
+        # Iterate over the frequencyCount array to make all values unique
+        for i in range(len(frequency_count)):
+            if frequency_count[i] <= 1:
+                continue
+
+            # Determine excess occurrences, carry them over to the next value,
+            # ensure single occurrence for current value, and update min_increments.
+            duplicates = frequency_count[i] - 1
+            frequency_count[i + 1] += duplicates
+            frequency_count[i] = 1
+            min_increments += duplicates
+
+        return min_increments
+        

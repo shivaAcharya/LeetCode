19169bfead6df08bd1a839a290107d2c70529594
Shiva Acharya <cva.acharya8@gmail.com>
2024-06-21 15:07:12 -0700

    Time: 217 ms (55.31%), Space: 18.9 MB (64.08%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         
-        satisfied = l = window = max_window = 0
+        # Find initially satisfied customers
+        satisfied = sum([c for c, g in zip(customers, grumpy) if not g])
         
+        # Use sliding window to find maximum unsatisfied cust that can be satisfied
+        l = window = max_window = 0
         for r, grump in enumerate(grumpy):
-            if not grump:
-                satisfied += customers[r]
-            else:
+            if grump:
                 window += customers[r]
                 
             if r - l  + 1 > minutes:

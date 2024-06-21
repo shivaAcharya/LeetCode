e5c6539e510bbbfe7384dacf6fd5b67791a5d1b8
Shiva Acharya <cva.acharya8@gmail.com>
2024-06-21 15:01:17 -0700

    Time: 218 ms (51.10%), Space: 19 MB (36.59%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        
+        satisfied = l = window = max_window = 0
+        
+        for r, grump in enumerate(grumpy):
+            if not grump:
+                satisfied += customers[r]
+            else:
+                window += customers[r]
+                
+            if r - l  + 1 > minutes:
+                if grumpy[l]:
+                    window -= customers[l]
+                l += 1
+            
+            max_window = max(max_window, window)
+            
+        return satisfied + max_window
+        

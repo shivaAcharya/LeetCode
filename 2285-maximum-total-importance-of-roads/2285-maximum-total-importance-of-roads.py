ee336e4d7481952eb12fabe6ef569aeab228ca99
Shiva Acharya <cva.acharya8@gmail.com>
2024-06-28 09:41:37 -0700

    Time: 1156 ms (100.00%), Space: 41.5 MB (87.70%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/maximum-total-importance-of-roads/solution/
+class Solution:
+    def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
+        degree = [0] * n
+
+        for edge in roads:
+            degree[edge[0]] += 1
+            degree[edge[1]] += 1
+
+        degree.sort()
+
+        value = 1
+        total_importance = 0
+        for d in degree:
+            total_importance += value * d
+            value += 1
+
+        return total_importance
+    
+        

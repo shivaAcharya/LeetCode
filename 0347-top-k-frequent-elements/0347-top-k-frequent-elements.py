089738417552d685d3e53fb280c9765257778d1a
Shiva Acharya <cva.acharya8@gmail.com>
2024-07-07 19:29:59 -0700

    Time: 92 ms (40.20%), Space: 22.2 MB (5.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -10,8 +10,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        # Bucket Sort
         counter = Counter(nums)
-        n_freq = heapq.nlargest(k, counter.values())
+        freq = [[] for _ in range(len(nums) + 1)]
         
-        return [u for u, v in counter.items() if v in n_freq]
+        for num, frq in counter.items():
+            freq[frq].append(num)
+        
+        # print(freq)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                # print(res, k)
+                if len(res) == k:
+                    return res                
         

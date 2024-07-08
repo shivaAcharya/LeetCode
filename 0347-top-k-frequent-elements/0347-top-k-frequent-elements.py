c34091fd29f7c070fa3008a0b94811215870ad69
Shiva Acharya <cva.acharya8@gmail.com>
2024-07-07 22:57:09 -0700

    Time: 86 ms (72.02%), Space: 22.2 MB (5.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,28 +1,35 @@
 """
+Can k > len(nums)? => No
+
+counter = {}
+max_heap
+nlargest(n, N) => O(N*logn)
+
 1 -> 3
 2 -> 2
+6 -> 2
 3 -> 1
 
-(3, 1), (2, 2), (1, 3)
+k = 2
 
+0, 1, 2, 3, 4   N
+[] [3] [2, 6] [1] [] []
+
+res = [2, 6]
 
 """
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        # Bucket Sort
         counter = Counter(nums)
         freq = [[] for _ in range(len(nums) + 1)]
         
         for num, frq in counter.items():
             freq[frq].append(num)
         
-        # print(freq)
         res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for num in freq[i]:
-                res.append(num)
-                # print(res, k)
+        for arr in reversed(freq):
+            for n in arr:
+                res.append(n)
                 if len(res) == k:
-                    return res                
-        
+                    return res

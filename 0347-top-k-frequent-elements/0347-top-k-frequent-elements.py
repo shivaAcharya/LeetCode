b3cc7dacd4ab3a98a9bd9e63a2a1a77a49376e14
Shiva Acharya <cva.acharya8@gmail.com>
2024-07-07 19:10:30 -0700

    Time: 85 ms (76.57%), Space: 21.3 MB (38.05%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,22 @@
+"""
+1 -> 3
+2 -> 2
+3 -> 1
+
+(3, 1), (2, 2), (1, 3)
+
+
+"""
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
         counter = Counter(nums)
+        heap = []
         
-        top_k_freq = heapq.nlargest(k, counter.values())
-                                    
-        res = []
+        for num, freq in counter.items():
+            heapq.heappush(heap, (freq, num))
+            
+        n_freq = heapq.nlargest(k, heap)
         
-        for k, v in counter.items():
-            if v in top_k_freq:
-                res.append(k)
+        return [v for _, v in n_freq]
         
-        return res

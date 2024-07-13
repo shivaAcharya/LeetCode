d47c94975a2f1517b7e017f1bf699306eec48d1b
Shiva Acharya <cva.acharya8@gmail.com>
2024-07-13 12:35:40 -0700

    Time: 89 ms (56.79%), Space: 22 MB (9.49%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,34 +1,34 @@
 """
-counter = {num -> freq}
 
-1 -> 3
-2 -> 2
-3 -> 1
--------
-4 -> 2
+counter = {
+    1 => 3,
+    2 => 2,
+    3 => 1
+}
 
-k = 2
-
-       0    1   2   3   4   5   6
 freq = [[], [3], [2], [1], [], [], []]
-res = [1]
+        0   1    2    3    4   5   6
 
+res = [1, 2]
 
+        
 """
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
         counter = Counter(nums)
         freq = [[] for _ in range(len(nums) + 1)]
         
-        for num, frq in counter.items():
-            freq[frq].append(num)
-        
+        for n, f in counter.items():
+            freq[f].append(n)
+            
         res = []
-        print(freq) # [[], [1]]
+        
         for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
-                print(res)
+                
                 if len(res) == k:
                     return res
-                
+        

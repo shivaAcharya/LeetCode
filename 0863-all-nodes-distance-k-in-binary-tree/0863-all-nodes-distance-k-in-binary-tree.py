2d711cc22ed774198e7ebedfdf177452a0d9f0c9
Shiva Acharya <cva.acharya8@gmail.com>
2024-07-23 16:23:47 -0700

    Time: 42 ms (37.27%), Space: 17 MB (21.83%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -15,33 +15,31 @@
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         
-        def populate_parent_map(node, parent):
+        def populate_parent(node, parent):
             if node:
                 parent_map[node] = parent
-                populate_parent_map(node.left, node)
-                populate_parent_map(node.right, node)
+                populate_parent(node.left, node)
+                populate_parent(node.right, node)
+
 
         def get_nodes(node, num_of_nodes_away):
-            if not node or node in traversed or num_of_nodes_away > k:
-                return
-            
+            if not node or num_of_nodes_away > k or node in traversed:
+                return 
+
             traversed.add(node)
             if num_of_nodes_away == k:
                 res.append(node.val)
-                return
-            
+
             get_nodes(node.left, num_of_nodes_away + 1)
             get_nodes(node.right, num_of_nodes_away + 1)
             get_nodes(parent_map[node], num_of_nodes_away + 1)
-            
-        
-        
+
+
         parent_map = {}
-        populate_parent_map(root, None)
         traversed = set()
         res = []
+        populate_parent(root, None)
         get_nodes(target, 0)
-        
         return res
-                
+
         

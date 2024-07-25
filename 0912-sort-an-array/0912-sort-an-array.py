29d1edd15cedbf2a860a97e2183807c30c790f31
Shiva Acharya <cva.acharya8@gmail.com>
2024-07-25 08:36:30 -0700

    Time: 1148 ms (48.06%), Space: 24 MB (77.62%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,36 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def merge_sort(nums):
+            if len(nums) > 1:
+
+                mid = len(nums) // 2
+                left_half = nums[:mid]
+                right_half = nums[mid:]
+
+                merge_sort(left_half)
+                merge_sort(right_half)
+
+                i = j = k = 0
+
+                while i < len(left_half) and j < len(right_half):
+                    if left_half[i] < right_half[j]:
+                        nums[k] = left_half[i]
+                        i += 1
+                    else:
+                        nums[k] = right_half[j]
+                        j += 1
+                    k += 1
+
+                while i < len(left_half):
+                    nums[k] = left_half[i]
+                    i += 1
+                    k += 1
+
+                while j < len(right_half):
+                    nums[k] = right_half[j]
+                    j += 1
+                    k += 1
+                    
+        merge_sort(nums)
+        return nums
+    

d2350a1e0593178b39e0e8600d93ca3a6077a684
Shiva Acharya <cva.acharya8@gmail.com>
2024-08-05 16:21:17 -0700

    Time: 579 ms (98.30%), Space: 21.5 MB (85.14%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,17 +1,17 @@
 class Solution:
-    def findJudge(self, N: int, trust: List[List[int]]) -> int:
-        if len(trust) < N - 1:
-            return -1
-        
-        if N == 1: return N
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        # Base case
+        if n == 1: return n
 
-        trust_scores = [0] * (N + 1)
+        trust_scores = [0] * (n + 1)
 
         for a, b in trust:
             trust_scores[a] -= 1
             trust_scores[b] += 1
 
-        for i, score in enumerate(trust_scores):
-            if score == N - 1:
-                return i
+        for cand, score in enumerate(trust_scores):
+            if score == n - 1:
+                return cand
+
         return -1
+    

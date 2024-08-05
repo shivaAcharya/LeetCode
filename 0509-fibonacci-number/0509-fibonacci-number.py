3d96778f8553cd46fa201cea957bb051d718ae78
Shiva Acharya <cva.acharya8@gmail.com>
2024-08-05 11:33:06 -0700

    Time: 37 ms (62.09%), Space: 16.4 MB (88.73%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,28 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
+#         if n == 0:
+#             return 0
     
-        f_0, f_1 = 0, 1
+#         f_0, f_1 = 0, 1
 
-        for _ in range(2, n):
-            f_0, f_1 = f_1, f_0 + f_1
+#         for _ in range(2, n):
+#             f_0, f_1 = f_1, f_0 + f_1
 
-        return f_0 + f_1
+#         return f_0 + f_1
+
+###### RECURSION WITH MEMOIZATION #######
+        memo = {}
+        
+        def dp(i):
+            if i in memo:
+                return memo[i]
+            if i == 0:
+                return 0
+            if i == 1:
+                return 1
+            
+            memo[i] = dp(i - 1) + dp(i - 2)
+            return memo[i]
+        
+        return dp(n)
+    

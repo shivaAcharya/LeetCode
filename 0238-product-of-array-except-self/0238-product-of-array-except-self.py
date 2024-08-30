417aa98e3d0d3049759d45e0c5c9d3f3cc335199
Shiva Acharya <cva.acharya8@gmail.com>
2024-08-30 14:10:04 -0700

    Time: 265 ms (67.83%), Space: 25.7 MB (48.61%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,52 @@
+"""
+nums = [1, 2, 3, 4]
+        ^ 
+prod = [24, 12, 8, 6]
+
+right_prod = 1 * 4 = 4 * 3 = 12 * 2 = 24
+
+left_prefix =  [1, 1, 2, 6]
+right_prefix = [24, 12, 4, 1]
+
+res = [24, 12, 8. 6]
+
+what if the array is empty? => return an empty array
+if the array contains one 0 =>
+if the array contains two 0s => return an array with all 0s
+
+
+
+"""
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = [1] * len(nums)
+        L = len(nums)
+        res = [1] * L
         
-        for i in range(1, len(nums)):
-            prod[i] = prod[i - 1] * nums[i - 1]
+        # Left traversal
+        for i in range(1, L):
+            res[i] = res[i - 1] * nums[i - 1]
         
-        cur_prod = 1
-        for i in range(len(nums) - 2, -1, -1):
-            cur_prod *= nums[i + 1]
-            prod[i] *= cur_prod
+        # Right traversal
+        prod = 1
+        for i in range(L - 2, -1, -1):
+            prod *= nums[i + 1]
+            res[i] *= prod
+        
+        return res
 
-        return prod
-        
+"""
+nums = [1, 2, 3, 4]
+           ^
+L = 4
+res = [24, 12, 8, 6]
+# Left
+1 -> 3
+
+# Right
+prod = 12
+i => 2 -> 0
+
+
+
+"""

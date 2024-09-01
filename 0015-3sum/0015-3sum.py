97ddfc94c1996f47ad13c95e4b162c9c8e14e7aa
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-01 14:47:11 -0700

    Time: 819 ms (31.43%), Space: 20.7 MB (72.18%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,10 +1,3 @@
-"""
-[-4, -1, -1, 0, 1, 2]
-          i  
-             j    
-                   k 
-
-"""
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
@@ -14,27 +7,20 @@ class Solution:
         for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            # Two Pointer
+                
             j, k = i + 1, len(nums) - 1
             
             while j < k:
-                if nums[i] + nums[j] + nums[k] < 0:     
-                    j += 1
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
-                elif nums[i] + nums[j] + nums[k] > 0:
-                    k -= 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
-                else:
+                cur_sum = nums[i] + nums[j] + nums[k]
+                
+                if cur_sum == 0:
                     res.append([nums[i], nums[j], nums[k]])
-                    j += 1
+                if cur_sum > 0:
                     k -= 1
+                else:
+                    j += 1
                     while j < k and nums[j] == nums[j - 1]:
                         j += 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
-        
+            
         return res
-        
+                

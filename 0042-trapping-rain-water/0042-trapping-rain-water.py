8610f8df34fc7d18c9733b3463442ec7d85ceb2c
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-03 14:50:21 -0700

    Time: 137 ms (11.58%), Space: 18.7 MB (10.72%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,32 @@
+"""
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+prefix = [0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
+postfix = [3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 1, 0]
+
+    i = 2
+water = min(max_l, max_r) - height[i] => (1) - 0 => 1
+    i = 5
+        2 - 0 => 2
+
+"""
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-        n = len(height)
-        left, right = 0, n - 1
-        left_max, right_max = height[left], height[right]
-        trapped_water = 0
-        while left < right:
-            if height[left] < height[right]:
-                left_max = max(left_max, height[left])
-                trapped_water += left_max - height[left]
-                left += 1
-            else:
-                right_max = max(right_max, height[right])
-                trapped_water += right_max - height[right]
-                right -= 1
-        return trapped_water
+        L = len(height)
+        prefix = [0] * L
+        postfix = [0] * L
+        
+        for i in range(1, L):
+            prefix[i] = max(height[i - 1], prefix[i - 1])
+        
+        for i in range(L - 2, -1, -1):
+            postfix[i] = max(height[i + 1], postfix[i + 1])
+        
+        # print(prefix, postfix)
+        res = 0
+        for i, h in enumerate(height):
+            water = max(min(prefix[i], postfix[i]) - h, 0)
+            # print()
+            res += water
+        
+        return res
+        

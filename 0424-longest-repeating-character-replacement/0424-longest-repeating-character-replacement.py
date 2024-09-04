6d0401f42c7213675bfb6aa0b56cffcc3609bcb0
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-04 13:02:56 -0700

    Time: 121 ms (29.23%), Space: 16.6 MB (74.80%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,27 +1,41 @@
 """
-
-s = A B A B
+s = A B A B       k = 2
     l
-      r
+          r
+    
+res = 4
+window = {
+    A : 2,
+    B : 2,
+}
 
-k = 2
-have = {A:1}
+
+s = A A B A B B A     k = 1
+      l
+              r
+    
+res = 4
+window = {
+    A : 3,
+    B : 3,
+}
 
 """
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        
         window = defaultdict(int)
         res = l = 0
         
         for r, c in enumerate(s):
             window[c] += 1
             
-            if (r - l + 1) - max(window.values()) > k:
+            while (r - l + 1) - max(window.values()) > k:
                 window[s[l]] -= 1
                 l += 1
             
-            res = max(res, r - l + 1)
+            res = max(res, (r - l + 1))
         
-        return res
-    
+        return res            
+        

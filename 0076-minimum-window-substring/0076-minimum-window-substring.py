c45179f7569b0c62ec35e5da50cebf6bde56f0d6
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-07 09:59:39 -0700

    Time: 118 ms (57.38%), Space: 17.3 MB (22.55%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,52 +1,31 @@
-"""
-Handle edge case of returning "" when len(t) > len(s).
-Initialize count_t and window hashmaps. Also res_len = inf, res = [-1, -1], need = len(t) and have = 0
-Populate count_t with chars from t.
-# Sliding window technique
-for r, c in enumerate(s):
-    Add c to window if c in count_t
-    check if window[c] == count_t, increment have
-    while have == need:
-        # update res
-        # move left pointer
-
-return s[l : r + 1] if res_len != inf
-
-"""
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
         if t == "": return ""
         
         count_t = Counter(t)
         window = {}
-        
         res, res_len = [-1, -1], float("inf")
-        have, need = 0, len(count_t)
+        need, have = len(count_t), 0
+        
         l = 0
-        # print(count_t)
         for r, c in enumerate(s):
             if c in count_t:
                 window[c] = 1 + window.get(c, 0)
-
-                if c in count_t and window[c] == count_t[c]:
+                
+                if window[c] == count_t[c]:
                     have += 1
-
-                while have == need:
-                    # Update res
+                    
+                while need == have:
                     if (r - l + 1) < res_len:
                         res_len = r - l + 1
                         res = [l, r]
-
-                    # Update l
+                    
                     if s[l] in count_t:
                         window[s[l]] -= 1
                         if window[s[l]] < count_t[s[l]]:
                             have -= 1
                     l += 1
-
-        # print(res)
-        l, r = res
-        return s[l : r + 1] if res_len != float("inf") else ""          
         
+        l, r = res
+        return s[l : r + 1] if res_len != float("inf") else ""
         

016256c519730535cac1aea8185fd16884e5342e
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-08 22:09:33 -0700

    Time: 752 ms (19.85%), Space: 56.3 MB (39.28%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# https://leetcode.com/problems/spiral-matrix-iv/solution/
+class Solution:
+    def spiralMatrix(self, m: int, n: int, head: "ListNode") -> List[List[int]]:
+        # Store the east, south, west, north movements in a matrix.
+        i = 0
+        j = 0
+        cur_d = 0
+        movement = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        res = [[-1 for _ in range(n)] for _ in range(m)]
+
+        while head is not None:
+            res[i][j] = head.val
+            newi = i + movement[cur_d][0]
+            newj = j + movement[cur_d][1]
+
+            # If we bump into an edge or an already filled cell, change the
+            # direction.
+            if (
+                min(newi, newj) < 0
+                or newi >= m
+                or newj >= n
+                or res[newi][newj] != -1
+            ):
+                cur_d = (cur_d + 1) % 4
+            i += movement[cur_d][0]
+            j += movement[cur_d][1]
+
+            head = head.next
+
+        return res
+        

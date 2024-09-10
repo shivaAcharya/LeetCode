4f7672c8019892880b399514d5a02f88c33ef220
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-09 22:00:28 -0700

    Time: 67 ms (71.57%), Space: 19.5 MB (97.91%) - LeetHub

---
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/solution/
+class Solution:
+    def insertGreatestCommonDivisors(
+        self, head: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        # Helper method to calculate the greatest common divisor using the Euclidean algorithm
+        def _calculate_gcd(a, b):
+            while b != 0:
+                a, b = b, a % b
+            return a
+
+        # If the list contains only one node, return the head as no insertion is needed
+        if not head.next:
+            return head
+
+        # Initialize pointers to traverse the list
+        node1 = head
+        node2 = head.next
+
+        # Traverse the linked list
+        while node2:
+            gcd_value = _calculate_gcd(node1.val, node2.val)
+            gcd_node = ListNode(gcd_value)
+
+            # Insert the GCD node between node1 and node2
+            node1.next = gcd_node
+            gcd_node.next = node2
+
+            # Move to the next pair of nodes
+            node1 = node2
+            node2 = node2.next
+
+        return head
+        

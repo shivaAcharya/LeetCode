6e42cff8c1a3296bff984591a06a0b040988f9f4
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-11 16:29:58 -0700

    Time: 34 ms (67.30%), Space: 16.6 MB (27.17%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/minimum-bit-flips-to-convert-number/solution/
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        count = 0
+        while start > 0 or goal > 0:
+            # Increment count if the current bits differ
+            if (start & 1) != (goal & 1):
+                count += 1
+            # Shift both numbers to the right to check the next bits
+            start >>= 1
+            goal >>= 1
+        return count

15a93240da4581bf2dfb97b955b7b76636f0782a
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-15 08:16:25 -0700

    Time: 289 ms (76.88%), Space: 22.2 MB (33.15%) - LeetHub

---
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -0,0 +1,27 @@
+"""
+
+l
+        r
+e l e e t m i n i c o w o r o e p
+
+
+"""
+# https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/solution/
+class Solution:
+    def findTheLongestSubstring(self, s: str) -> int:
+        prefixXOR = 0
+        characterMap = [0] * 26
+        characterMap[ord("a") - ord("a")] = 1
+        characterMap[ord("e") - ord("a")] = 2
+        characterMap[ord("i") - ord("a")] = 4
+        characterMap[ord("o") - ord("a")] = 8
+        characterMap[ord("u") - ord("a")] = 16
+        mp = [-1] * 32
+        longestSubstring = 0
+        for i in range(len(s)):
+            prefixXOR ^= characterMap[ord(s[i]) - ord("a")]
+            if mp[prefixXOR] == -1 and prefixXOR != 0:
+                mp[prefixXOR] = i
+            longestSubstring = max(longestSubstring, i - mp[prefixXOR])
+        return longestSubstring
+        

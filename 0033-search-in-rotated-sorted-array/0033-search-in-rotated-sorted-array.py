97da49ff37e8f9cba98df50a31d95a20744bc71a
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-15 10:20:05 -0700

    Time: 37 ms (90.09%), Space: 17.1 MB (5.16%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,19 +1,8 @@
-"""
-Find pivot index
-Figure out what side of pivot index is the target located
-Find the target element
-
-               l
-               m
-                    r
-nums = [4,5,6,7,0,1,2], target = 0
-
-
-"""
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Find Pivot index
-        l, r = 0, len(nums) - 1
+        
+        # Find pivot
+        l, r = 0, len(nums) -  1
         
         while l < r:
             mid = (l + r) // 2
@@ -22,16 +11,14 @@ class Solution:
             else:
                 r = mid
         
-        # print(l)
-        # Check Pivot
+        # Check pivot
         idx = l
         l, r = 0, len(nums) - 1
         if nums[r] >= target:
             l = idx
         else:
             r = idx
-        
-        # print(l, r)
+            
         while l < r:
             mid = (l + r) // 2
             if nums[mid] < target:
@@ -40,8 +27,4 @@ class Solution:
                 r = mid
         
         return l if nums[l] == target else -1
-"""
-[1, 3]
-
-
-"""    
+            

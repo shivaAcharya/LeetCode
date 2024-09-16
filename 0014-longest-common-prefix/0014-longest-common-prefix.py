d298364e1eaa25b565fc6968cdb8799a974eb07c
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-16 12:37:57 -0700

    Time: 40 ms (40.65%), Space: 16.8 MB (30.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,20 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
-        lcp = strs[0]
-        
-        for i in range(len(strs) - 1):
-            word1, word2 = strs[i], strs[i + 1]
-            
-            if not word1 or not word2: return ""
-            if word1[0] != word2[0]: return ""
-            
-            prefix = []
-            for c1, c2 in zip(word1, word2):
-                if c1 != c2: break
-                prefix.append(c1)
-            
-            if len(prefix) < len(lcp):
-                lcp = "".join(prefix)
-        
-        return lcp
+        res = strs[0]
+
+        for s in strs[1:]:
+            # If s < res, update res to s
+            if len(s) < len(res):
+                res = res[:len(s)]
+            for i, c in enumerate(s):
+                if i == len(res) or res[i] != c:
+                    res = res[:i]
+                    break
+
+        return "".join(res)

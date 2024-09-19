4fbb7091d38395bacf50eed820b2f748c0b1c6e9
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-19 10:40:12 -0700

    Time: 51 ms (51.82%), Space: 16.7 MB (16.93%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,33 +1,30 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
         ROWS, COLS = len(grid), len(grid[0])
-        
         Q = deque()
-        seen = set()
         fresh_oranges = 0
-        
-        # Populate Q with rotten oranges
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
                     Q.append((r, c))
-                    seen.add((r, c))
-                if grid[r][c] == 1:
+                elif grid[r][c] == 1:
                     fresh_oranges += 1
-                    
-        minutes = 0
+        if not fresh_oranges:
+            return 0
         
+        minutes = -1
         while Q:
-            rotten_oranges = []
-            for r, c in Q:
-                for row, col in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
-                    if 0 <= row < ROWS and 0 <= col < COLS and (row, col) not in seen and grid[row][col] == 1:
-                        rotten_oranges.append((row, col))
-                        seen.add((row, col))
-                        fresh_oranges -= 1 
-            minutes += 1 if rotten_oranges else 0
-            Q = rotten_oranges
+            # print(Q, fresh_oranges, minutes)
+            for _ in range(len(Q)):
+                r, c = Q.popleft()
+                
+                for nr, nc in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
+                    if 0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh_oranges -= 1
+                        Q.append((nr, nc))
+        
+            minutes += 1
         
         return minutes if not fresh_oranges else -1
         

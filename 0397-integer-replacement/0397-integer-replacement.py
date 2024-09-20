aaa341da6524b85bf338dc046a785d3fef5a3636
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-19 17:36:22 -0700

    Time: 35 ms (66.36%), Space: 16.4 MB (87.27%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.py
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -4,12 +4,12 @@ class Solution:
             return 0
         level = 0
         while n != 1:
-            if n & 1 == 0:
+            if n % 2 == 0:
                 n, level = n // 2, level + 1
             elif n == 3:
                 n, level = n - 1, level + 1
             else:
-                if (n - 1) // 2 & 1 == 0:
+                if ((n - 1) // 2) % 2 == 0:
                     n, level = n - 1, level + 1
                 else:
                     n, level = n + 1, level + 1

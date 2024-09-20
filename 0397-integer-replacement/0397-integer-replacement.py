fd8d3b9ea6981bfb9522db9dff634ae50fae50ef
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-19 17:29:34 -0700

    Time: 28 ms (93.84%), Space: 16.7 MB (23.03%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -0,0 +1,17 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        if n <= 1:
+            return 0
+        level = 0
+        while n != 1:
+            if n & 1 == 0:
+                n, level = n // 2, level + 1
+            elif n == 3:
+                n, level = n - 1, level + 1
+            else:
+                if (n - 1) // 2 & 1 == 0:
+                    n, level = n - 1, level + 1
+                else:
+                    n, level = n + 1, level + 1
+        return level
+        

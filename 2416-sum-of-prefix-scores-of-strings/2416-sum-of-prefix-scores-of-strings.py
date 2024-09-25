bb5239639c84c97e0edbd608407ead1679475a83
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-25 11:19:59 -0700

    Time: 5069 ms (5.18%), Space: 598.1 MB (15.17%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -1,45 +1,64 @@
-class TrieNode:
+# class TrieNode:
     
-    def __init__(self):
-        self.children = {}
-        self.count = 0
+#     def __init__(self):
+#         self.children = {}
+#         self.count = 0
         
 
-class Trie:
+# class Trie:
     
-    def __init__(self):
-        self.root = TrieNode()
+#     def __init__(self):
+#         self.root = TrieNode()
         
     
-    def insert(self, word):
-        cur = self.root
+#     def insert(self, word):
+#         cur = self.root
         
-        for c in word:
-            if c not in cur.children:
-                cur.children[c] = TrieNode()
+#         for c in word:
+#             if c not in cur.children:
+#                 cur.children[c] = TrieNode()
             
-            cur = cur.children[c]
-            cur.count += 1
+#             cur = cur.children[c]
+#             cur.count += 1
     
     
-    def count(self, word):
-        res = 0
-        cur = self.root
-        for c in word:
-            if c in cur.children:
-                cur = cur.children[c]
-                res += cur.count
+#     def count(self, word):
+#         res = 0
+#         cur = self.root
+#         for c in word:
+#             if c in cur.children:
+#                 cur = cur.children[c]
+#                 res += cur.count
 
-        return res
+#         return res
 
 
 
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
         
-        trie = Trie()
+#         trie = Trie()
+        
+#         for word in words:
+#             trie.insert(word)
+        
+#         # return [trie.count(word) for word in words]
+        
+#         return map(trie.count, words)
+        pre_freq = Counter()
+        res = []
         
         for word in words:
-            trie.insert(word)
+            for i in range(len(word)):
+                pre_freq[word[:i + 1]] += 1
         
-        return map(trie.count, words)
+        for word in words:
+            score = 0
+            for i in range(len(word)):
+                if word[:i + 1] not in pre_freq:
+                    break
+                score += pre_freq[word[:i + 1]]
+            
+            res.append(score)
+            
+        return res

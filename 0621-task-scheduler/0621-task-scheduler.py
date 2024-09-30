34987b0ab477e2fd882756b05435277a35f9849f
Shiva Acharya <cva.acharya8@gmail.com>
2024-09-29 17:51:32 -0700

    Time: 475 ms (45.63%), Space: 17.1 MB (38.81%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,42 +1,34 @@
-"""
-## RC ##
-## APPROACH : HASHMAP ##
-## LOGIC : TAKE THE MAXIMUM FREQUENCY ELEMENT AND MAKE THOSE MANY NUMBER OF SLOTS ##
-##  Slot size = (n+1) if n= 2 => slotsize = 3 Example: {A:5, B:1} => ABxAxxAxxAxxAxx => indices of A = 0,2 and middle there should be n elements, so slot size should be n+1
-
-## Ex: {A:6,B:4,C:2} n = 2
-## final o/p will be
-## slot size / cycle size = 3
-## Number of rows = number of A's (most freq element)
-# [
-#     [A, B,      C],
-#     [A, B,      C],
-#     [A, B,      idle],
-#     [A, B,      idle],
-#     [A, idle,   idle],
-#     [A   -        - ],
-# ]
-#
-# so from above total time intervals = (max_freq_element - 1) * (n + 1) + (all elements with max freq)
-                             # ans   =     rows_except_last   * columns +        last_row
-
-
-## but consider {A:5, B:1, C:1, D:1, E:1, F:1, G:1, H:1, I:1, J:1, K:1, L:1} n = 1
-## total time intervals by above formula will be 4 * 2 + 1 = 9, which is less than number of elements, which is not possible. so we have to return max(ans, number of tasks)
-
-
-## TIME COMPLEXITY : O(N) ##
-## SPACE COMPLEXITY : O(1) ##
-
-"""
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        
         counter = Counter(tasks)
-        freq = list(counter.values())
-        max_freq = max(freq)
-        max_freq_count = freq.count(max_freq)
+        max_heap = [-cnt for cnt in counter.values()]
+        heapq.heapify(max_heap)
         
-        ans = (max_freq - 1) * (n + 1) + max_freq_count
+        Q = deque() # [-cnt, cooldown]
+        time = 0
         
-        return max(ans, len(tasks))
+        while max_heap or Q:
+            
+            time += 1
+            
+            if max_heap:
+                cnt = 1 + heapq.heappop(max_heap)
+                if cnt:
+                    Q.append((cnt, time + n))
+            
+            if Q and Q[0][1] == time:
+                heapq.heappush(max_heap, Q.popleft()[0])
+        
+        return time
+                               
+    """
+    
+    max_heap = [-2]
+    Q = [(-2, 3), (-1)]
+    time = 4
+    while 
+        cnt = -1
+    
+    """
         

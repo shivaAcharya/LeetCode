3ea12ba8703f8c887d6000d132c0f4e03182f58d
Shiva Acharya <cva.acharya8@gmail.com>
2024-10-02 11:59:18 -0700

    Time: 51 ms (51.59%), Space: 16.6 MB (61.99%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,28 +3,26 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
         Q = deque()
         fresh_oranges = 0
+        
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 2:
-                    Q.append((r, c))
-                elif grid[r][c] == 1:
+                if grid[r][c] == 1:
                     fresh_oranges += 1
-        # if not fresh_oranges:
-        #     return 0
-        
+                elif grid[r][c] == 2:
+                    Q.append((r, c))
+                    
         minutes = 0
-        while Q and fresh_oranges > 0:
-            # print(Q, fresh_oranges, minutes)
+        
+        while Q:
             for _ in range(len(Q)):
                 r, c = Q.popleft()
-                
                 for nr, nc in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
                     if 0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] == 1:
                         grid[nr][nc] = 2
                         fresh_oranges -= 1
                         Q.append((nr, nc))
-        
-            minutes += 1
+            if Q:
+                minutes += 1
         
         return minutes if not fresh_oranges else -1
         

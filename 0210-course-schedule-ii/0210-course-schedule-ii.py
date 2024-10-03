2b8bde27795884a6d4dc68c959d267defb8493a8
Shiva Acharya <cva.acharya8@gmail.com>
2024-10-02 18:00:42 -0700

    Time: 94 ms (36.25%), Space: 18.3 MB (82.29%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,22 +1,27 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        G = defaultdict(set)
-        indegrees = Counter()
+        # if not prerequisites: return []
         
-        for a, b in prerequisites:
-            G[b].add(a)
-            indegrees[a] += 1
-            
-        Q = deque([course for course in range(numCourses) if course not in indegrees])
+        G = defaultdict(list)
+        indegrees = [0] * numCourses
+        
+        # Build G
+        for u, v in prerequisites:
+            G[v].append(u)
+            indegrees[u] += 1
+        
+        Q = deque([idx for idx, course in enumerate(indegrees) if course == 0])
         res = []
         
         while Q:
             course = Q.popleft()
             res.append(course)
+            
             for next_course in G[course]:
                 indegrees[next_course] -= 1
                 if indegrees[next_course] == 0:
                     Q.append(next_course)
+        
         return res if len(res) == numCourses else []
         

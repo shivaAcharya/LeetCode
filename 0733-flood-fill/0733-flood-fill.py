29549071ad468d5a01e6226e13878e20c5ced98a
Shiva Acharya <cva.acharya8@gmail.com>
2024-10-21 22:12:01 -0700

    Time: 0 ms (100.00%), Space: 16.8 MB (51.74%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,18 +1,17 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
         ROWS, COLS = len(image), len(image[0])
-        start_color = image[sr][sc]
-        flood_fill_image = deepcopy(image)
+        source_color = image[sr][sc]
+        Q = deque([(sr, sc)])
+        seen = set([(sr, sc)])
+        while Q:
+            for _ in range(len(Q)):
+                r, c = Q.popleft()
+                image[r][c] = color
+                for row, col in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
+                    if 0 <= row < ROWS and 0 <= col < COLS and (row, col) not in seen and image[row][col] == source_color:
+                        Q.append((row, col))
+                        seen.add((row, col))
+                        
+        return image
         
-        # DFS
-        def dfs(r, c):
-            flood_fill_image[r][c] = color
-            image[r][c] = -1 # mark visited
-            
-            for nr, nc in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
-                if 0 <= nr < ROWS and 0 <= nc < COLS and image[nr][nc] == start_color:
-                    dfs(nr, nc)
-        
-        dfs(sr, sc)
-        return flood_fill_image
